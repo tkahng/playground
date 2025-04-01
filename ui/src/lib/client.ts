3f4498892dfa84548c6cc340199cce0a88363f14
tkahng <tkahng@gmail.com>
2025-04-01 09:54:51 -0700

    fix url

--- ui/src/lib/client.ts
+++ ui/src/lib/client.ts
@@ -2,5 +2,5 @@ import { paths } from "@/schema";
 import createClient from "openapi-fetch";
 
 export const client = createClient<paths>({
-  baseUrl: "http://localhost:8080/",
+  baseUrl: "/",
 });
