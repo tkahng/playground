9e643afb14f1b8833a56c362009509ea014fa323
tkahng <tkahng@gmail.com>
2025-04-13 17:42:04 -0700

    change types

--- internal/shared/auth.dto.go
+++ internal/shared/auth.dto.go
@@ -69,28 +69,6 @@ type TokenDto struct {
 	RefreshToken string `json:"refresh_token"`
 }
 
-type User struct {
-	ID              uuid.UUID  `db:"id,pk" json:"id"`
-	Email           string     `db:"email" json:"email"`
-	EmailVerifiedAt *time.Time `db:"email_verified_at" json:"email_verified_at"`
-	Name            *string    `db:"name" json:"name"`
-	Image           *string    `db:"image" json:"image"`
-	CreatedAt       time.Time  `db:"created_at" json:"created_at"`
-	UpdatedAt       time.Time  `db:"updated_at" json:"updated_at"`
-}
-
-func ToUser(user *models.User) *User {
-	return &User{
-		ID:              user.ID,
-		Email:           user.Email,
-		EmailVerifiedAt: user.EmailVerifiedAt.Ptr(),
-		Name:            user.Name.Ptr(),
-		Image:           user.Image.Ptr(),
-		CreatedAt:       user.CreatedAt,
-		UpdatedAt:       user.UpdatedAt,
-	}
-}
-
 type AuthenticatedDTO struct {
 	User        *User              `json:"user"`
 	Permissions []string           `json:"permissions"`
