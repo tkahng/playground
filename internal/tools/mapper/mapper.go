7d5212360927e3d2c0a194f82248f944edd113fb
tkahng <tkahng@gmail.com>
2025-04-17 20:45:25 -0700

    rename

--- internal/tools/mapper/mapper.go
+++ internal/tools/mapper/mapper.go
@@ -1,21 +1,6 @@
 package mapper
 
-// func MapTo[]
-
 type KeyFn[T any, K comparable] func(T) K
-
-// These helper functions are intended to be used in data loaders for mapping
-// entity keys to entity values (db records). See `../context.ts`.
-// https://github.com/graphql/dataloader
-
-// export function mapTo<R, K>(
-//   records: ReadonlyArray<R>,
-//   keys: ReadonlyArray<K>,
-//   keyFn: (record: R) => K,
-// ): Array<R | null> {
-//   const map = new Map(records.map((x) => [keyFn(x), x]));
-//   return keys.map((key) => map.get(key) || null);
-// }
 
 func MapTo[T any, K comparable](records []T, keys []K, keyFn KeyFn[T, K]) []T {
 	var m = make(map[K]T)
@@ -32,17 +17,6 @@ func MapTo[T any, K comparable](records []T, keys []K, keyFn KeyFn[T, K]) []T {
 	return result
 }
 
-// export function mapToMany<R, K>(
-//
-//	records: ReadonlyArray<R>,
-//	keys: ReadonlyArray<K>,
-//	keyFn: (record: R) => K,
-//
-//	): Array<R[]> {
-//	  const group = new Map<K, R[]>(keys.map((key) => [key, []]));
-//	  records.forEach((record) => (group.get(keyFn(record)) || []).push(record));
-//	  return Array.from(group.values());
-//	}
 func MapToMany[T any, K comparable](records []T, keys []K, keyFn KeyFn[T, K]) [][]T {
 	var m = make(map[K][]T)
 	for _, record := range records {
