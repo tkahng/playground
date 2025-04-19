eb185ab181ceabea0ec1afe4eb1bcd993f886cdd
tkahng <tkahng@gmail.com>
2025-04-19 08:09:46 -0700

    ai usage repo

---
+++ internal/shared/ai_usage.go
@@ -0,0 +1,43 @@
+package shared
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tkahng/authgo/internal/db/models"
+)
+
+type AiUsage struct {
+	ID               uuid.UUID `db:"id,pk" json:"id"`
+	UserID           uuid.UUID `db:"user_id" json:"user_id"`
+	PromptTokens     int64     `db:"prompt_tokens" json:"prompt_tokens"`
+	CompletionTokens int64     `db:"completion_tokens" json:"completion_tokens"`
+	TotalTokens      int64     `db:"total_tokens" json:"total_tokens"`
+	CreatedAt        time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt        time.Time `db:"updated_at" json:"updated_at"`
+}
+
+func ModelToAiUsage(aiUsage *models.AiUsage) *AiUsage {
+	return &AiUsage{
+		ID:               aiUsage.ID,
+		UserID:           aiUsage.UserID,
+		PromptTokens:     aiUsage.PromptTokens,
+		CompletionTokens: aiUsage.CompletionTokens,
+		TotalTokens:      aiUsage.TotalTokens,
+		CreatedAt:        aiUsage.CreatedAt,
+		UpdatedAt:        aiUsage.UpdatedAt,
+	}
+}
+
+type AiUsageListFilter struct {
+	Q      string   `query:"q,omitempty" required:"false"`
+	UserID string   `query:"user_id,omitempty" required:"false" format:"uuid"`
+	Ids    []string `query:"ids,omitempty,explode" required:"false" minimum:"1" maximum:"100" format:"uuid"`
+}
+
+type AiUsageListParams struct {
+	PaginatedInput
+	AiUsageListFilter
+	SortParams
+	Expand []string `query:"expand,omitempty" required:"false" minimum:"1" maximum:"100" enum:"subtasks"`
+}
