5d15dc8ea70b75c793ea73f715e7e7b240d698bd
tkahng <tkahng@gmail.com>
2025-04-23 00:13:04 -0700

    remove comment

--- internal/core/app.go
+++ internal/core/app.go
@@ -19,19 +19,6 @@ type App interface {
 	NewMailClient() mailer.Mailer
 	EncryptionEnv() string
 
-	// jwt
-	// CreateAuthTokens(ctx context.Context, db bob.Executor, payload *shared.UserInfoDto) (*shared.TokenDto, error)
-	// CreateAuthDto(ctx context.Context, email string) (*shared.AuthenticatedDTO, error)
-	// HandleAuthToken(ctx context.Context, token string) (*shared.UserInfoDto, error)
-	// RefreshTokens(ctx context.Context, db bob.Executor, refreshToken string) (*shared.AuthenticatedDTO, error)
-	// verification
-	// SendVerificationEmail(ctx context.Context, db bob.Executor, user *models.User, redirectTo string) error
-	// VerifyAndUsePasswordResetToken(ctx context.Context, db bob.Executor, token string) (*PasswordResetClaims, error)
-	// SendPasswordResetEmail(ctx context.Context, db bob.Executor, user *models.User, redirectTo string) error
-
-	// SendSecurityPasswordResetEmail(ctx context.Context, db bob.Executor, user *models.User, redirectTo string) error
-	// CheckUserCredentialsSecurity(ctx context.Context, db bob.Executor, user *models.User, params *shared.AuthenticateUserParams) error
-	// stripe
 	Payment() *StripeService
 
 	NewAuthActions(db bob.Executor) AuthActions
