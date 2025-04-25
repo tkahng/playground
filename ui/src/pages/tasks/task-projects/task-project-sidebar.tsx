a2776449a083296c4d79e08df515ca066c059134
tkahng <tkahng@gmail.com>
2025-04-24 19:59:21 -0700

    sidebar

--- ui/src/pages/tasks/task-projects/task-project-sidebar.tsx
+++ ui/src/pages/tasks/task-projects/task-project-sidebar.tsx
@@ -1,9 +1,9 @@
+import { Separator } from "@/components/ui/separator";
 import { useAuthProvider } from "@/hooks/use-auth-provider";
 import { taskProjectList } from "@/lib/queries";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { Link, useLocation } from "react-router";
-
 export default function TaskProjectSidebar() {
   const { user: auth } = useAuthProvider();
   const { pathname } = useLocation();
@@ -15,14 +15,14 @@ export default function TaskProjectSidebar() {
   } = useQuery({
     queryKey: ["recent-projects"],
     select(data) {
-      return data.data;
+      return data;
     },
     queryFn: async () => {
       if (!auth?.tokens.access_token) {
         throw new Error("Missing access token or role ID");
       }
       const data = await taskProjectList(auth.tokens.access_token, {
-        page: 1,
+        page: 0,
         per_page: 5,
         sort_by: "updated_at",
         sort_order: "desc",
@@ -41,30 +41,40 @@ export default function TaskProjectSidebar() {
   }
   return (
     <>
-      {/* <div>
-        {projects?.map((project) => (
-          <Link key={project.id} to={`/projects/${project.id}`}>
-            {project.name}
-          </Link>
-        ))}
-      </div> */}
       <nav className="flex flex-col w-64 py-8 space-y-2 justify-start border-r grow-0">
-        {/* {backLink && <BackLink to={backLink.to} name={backLink.title} />} */}
-        {projects?.map((item) => (
+        <div className="flex flex-col pr-4">
+          <h3 className="text-md font-medium">Recent Projects</h3>
+        </div>
+        <div className="flex flex-col pr-4">
+          <Separator />
+        </div>
+        {projects?.data?.map((item) => (
           <Link
             key={item.id}
             to={`/projects/${item.id}`}
             className={cn(
-              // buttonVariants({ variant: "ghost" }),
               pathname.startsWith(`/projects/${item.id}`)
                 ? "underline"
                 : "text-muted-foreground",
-              "text-sm font-normal hover:text-primary transition-colors hover:bg-muted rounded-md p-2"
+              "text-md font-normal hover:text-primary transition-colors hover:bg-muted rounded-md p-2"
             )}
           >
             <span>{item.name}</span>
           </Link>
         ))}
+        <div className="flex flex-col pr-4">
+          <Separator />
+        </div>
+        {projects?.meta.has_more && (
+          <Link
+            to={`/projects`}
+            className={cn(
+              "text-sm font-normal hover:text-primary transition-colors hover:bg-muted rounded-md p-2 flex"
+            )}
+          >
+            View all projects
+          </Link>
+        )}
       </nav>
     </>
   );
