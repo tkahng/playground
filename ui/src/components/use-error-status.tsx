b32916b757e7e23bd62282699d6df93a7037e134
tkahng <tkahng@gmail.com>
2025-04-25 10:24:43 -0700

    fix error status

--- ui/src/components/use-error-status.tsx
+++ ui/src/components/use-error-status.tsx
@@ -1,6 +1,5 @@
 // react hook to set errors
-import { useState } from "react";
 
 export const useErrorStatus = () => {
-  const [error, setError] = useState<string | null>(null);
+  // const [error, setError] = useState<string | null>(null);
 };
