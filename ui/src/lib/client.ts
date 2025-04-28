ecb4c26c69fabf259f6b5a89ad9f9448bb9599a5
tkahng <tkahng@gmail.com>
2025-04-27 21:17:33 -0700

    serialization

--- ui/src/lib/client.ts
+++ ui/src/lib/client.ts
@@ -3,4 +3,14 @@ import createClient from "openapi-fetch";
 
 export const client = createClient<paths>({
   baseUrl: "/",
+  querySerializer: {
+    array: {
+      style: "form", // "form" (default) | "spaceDelimited" | "pipeDelimited"
+      explode: false,
+    },
+    object: {
+      style: "deepObject", // "form" | "deepObject" (default)
+      explode: true,
+    },
+  },
 });
