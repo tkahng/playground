446bbe68a219b2b566d1973eff97d65e2d4bc80e
tkahng <tkahng@gmail.com>
2025-04-28 18:18:04 -0700

    cleanup

--- internal/repository/token_repository_test.go
+++ internal/repository/token_repository_test.go
@@ -2,7 +2,6 @@ package repository_test
 
 import (
 	"context"
-	"reflect"
 	"testing"
 	"time"
 
@@ -71,37 +70,6 @@ func TestCreateToken(t *testing.T) {
 	}
 }
 
-func TestUseToken(t *testing.T) {
-	ctx, dbx, pl := test.DbSetup()
-	t.Cleanup(func() {
-		repository.TruncateModels(ctx, dbx)
-		pl.Close()
-	})
-	type args struct {
-		params string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *models.Token
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := repository.UseToken(ctx, dbx, tt.args.params)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UseToken() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("UseToken() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func randomToken(ctx context.Context, db bob.Executor, email string, tokenType models.TokenTypes, otp string) (*models.Token, error) {
 	token := &models.TokenSetter{
 		Identifier: omit.From(email),
@@ -113,110 +81,110 @@ func randomToken(ctx context.Context, db bob.Executor, email string, tokenType m
 	return models.Tokens.Insert(token).One(ctx, db)
 }
 
-func TestDeleteTokensByUser(t *testing.T) {
-	ctx, dbx, pl := test.DbSetup()
-	t.Cleanup(func() {
-		repository.TruncateModels(ctx, dbx)
-		pl.Close()
-	})
-	identifier := "email@example.com"
-	t1, _ := randomToken(ctx, dbx, identifier, models.TokenTypesReauthenticationToken, security.GenerateOtp(6))
-	t2, _ := randomToken(ctx, dbx, identifier, models.TokenTypesPasswordResetToken, security.GenerateOtp(6))
-	t3, _ := randomToken(ctx, dbx, identifier, models.TokenTypesRefreshToken, security.GenerateOtp(6))
-	t4, _ := randomToken(ctx, dbx, identifier, models.TokenTypesStateToken, security.GenerateOtp(6))
-	t5, _ := randomToken(ctx, dbx, identifier, models.TokenTypesVerificationToken, security.GenerateOtp(6))
-	type args struct {
-		ctx    context.Context
-		db     bob.Executor
-		params *repository.OtpDto
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int64
-		wantErr bool
-	}{
-		{
-			name: "",
-			args: args{
-				ctx: ctx,
-				db:  dbx,
-				params: &repository.OtpDto{
-					Type:       t5.Type,
-					Identifier: t5.Identifier,
-					Otp:        t5.Otp.Ptr(),
-				},
-			},
-			want:    4,
-			wantErr: false,
-		},
-		{
-			name: "",
-			args: args{
-				ctx: ctx,
-				db:  dbx,
-				params: &repository.OtpDto{
-					Type:       t4.Type,
-					Identifier: t4.Identifier,
-					Otp:        t4.Otp.Ptr(),
-				},
-			},
-			want:    3,
-			wantErr: false,
-		},
-		{
-			name: "",
-			args: args{
-				ctx: ctx,
-				db:  dbx,
-				params: &repository.OtpDto{
-					Type:       t3.Type,
-					Identifier: t3.Identifier,
-					Otp:        t3.Otp.Ptr(),
-				},
-			},
-			want:    2,
-			wantErr: false,
-		},
-		{
-			name: "",
-			args: args{
-				ctx: ctx,
-				db:  dbx,
-				params: &repository.OtpDto{
-					Type:       t2.Type,
-					Identifier: t2.Identifier,
-					Otp:        t2.Otp.Ptr(),
-				},
-			},
-			want:    1,
-			wantErr: false,
-		},
-		{
-			name: "",
-			args: args{
-				ctx: ctx,
-				db:  dbx,
-				params: &repository.OtpDto{
-					Type:       t1.Type,
-					Identifier: t1.Identifier,
-					Otp:        t1.Otp.Ptr(),
-				},
-			},
-			want:    0,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+// func TestDeleteTokensByUser(t *testing.T) {
+// 	ctx, dbx, pl := test.DbSetup()
+// 	t.Cleanup(func() {
+// 		repository.TruncateModels(ctx, dbx)
+// 		pl.Close()
+// 	})
+// 	identifier := "email@example.com"
+// 	t1, _ := randomToken(ctx, dbx, identifier, models.TokenTypesReauthenticationToken, security.GenerateOtp(6))
+// 	t2, _ := randomToken(ctx, dbx, identifier, models.TokenTypesPasswordResetToken, security.GenerateOtp(6))
+// 	t3, _ := randomToken(ctx, dbx, identifier, models.TokenTypesRefreshToken, security.GenerateOtp(6))
+// 	t4, _ := randomToken(ctx, dbx, identifier, models.TokenTypesStateToken, security.GenerateOtp(6))
+// 	t5, _ := randomToken(ctx, dbx, identifier, models.TokenTypesVerificationToken, security.GenerateOtp(6))
+// 	type args struct {
+// 		ctx    context.Context
+// 		db     bob.Executor
+// 		params *repository.OtpDto
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		want    int64
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name: "",
+// 			args: args{
+// 				ctx: ctx,
+// 				db:  dbx,
+// 				params: &repository.OtpDto{
+// 					Type:       t5.Type,
+// 					Identifier: t5.Identifier,
+// 					Otp:        t5.Otp.Ptr(),
+// 				},
+// 			},
+// 			want:    4,
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "",
+// 			args: args{
+// 				ctx: ctx,
+// 				db:  dbx,
+// 				params: &repository.OtpDto{
+// 					Type:       t4.Type,
+// 					Identifier: t4.Identifier,
+// 					Otp:        t4.Otp.Ptr(),
+// 				},
+// 			},
+// 			want:    3,
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "",
+// 			args: args{
+// 				ctx: ctx,
+// 				db:  dbx,
+// 				params: &repository.OtpDto{
+// 					Type:       t3.Type,
+// 					Identifier: t3.Identifier,
+// 					Otp:        t3.Otp.Ptr(),
+// 				},
+// 			},
+// 			want:    2,
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "",
+// 			args: args{
+// 				ctx: ctx,
+// 				db:  dbx,
+// 				params: &repository.OtpDto{
+// 					Type:       t2.Type,
+// 					Identifier: t2.Identifier,
+// 					Otp:        t2.Otp.Ptr(),
+// 				},
+// 			},
+// 			want:    1,
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "",
+// 			args: args{
+// 				ctx: ctx,
+// 				db:  dbx,
+// 				params: &repository.OtpDto{
+// 					Type:       t1.Type,
+// 					Identifier: t1.Identifier,
+// 					Otp:        t1.Otp.Ptr(),
+// 				},
+// 			},
+// 			want:    0,
+// 			wantErr: false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
 
-			if err := repository.DeleteTokensByUser(tt.args.ctx, tt.args.db, tt.args.params); (err != nil) != tt.wantErr {
-				t.Errorf("DeleteTokensByUser() error = %v, wantErr %v", err, tt.wantErr)
-			}
-			count, _ := models.Tokens.Query().Count(tt.args.ctx, tt.args.db)
-			if count != tt.want {
-				t.Errorf("DeleteTokensByUser() = %v, want %v", count, tt.want)
-			}
-		})
-	}
-}
+// 			if err := repository.DeleteTokensByUser(tt.args.ctx, tt.args.db, tt.args.params); (err != nil) != tt.wantErr {
+// 				t.Errorf("DeleteTokensByUser() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
+// 			count, _ := models.Tokens.Query().Count(tt.args.ctx, tt.args.db)
+// 			if count != tt.want {
+// 				t.Errorf("DeleteTokensByUser() = %v, want %v", count, tt.want)
+// 			}
+// 		})
+// 	}
+// }
