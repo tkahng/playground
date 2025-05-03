9c415a6a7f6cb4696aa18935f8df982652233a0d
tkahng <tkahng@gmail.com>
2025-05-03 16:38:53 -0700

    cleanupdb

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,77 +2,24 @@ package db
 
 import (
 	"context"
-	"database/sql"
-	"database/sql/driver"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/stephenafamo/scan"
 )
 
 type DBTX interface {
-	// Begin(ctx context.Context) (pgx.Tx, error)
-	// BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
 	Query(ctx context.Context, sql string, arguments ...any) (pgx.Rows, error)
-	QueryRow(ctx context.Context, sql string, arguments ...any) pgx.Row
 	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
-	// SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
-	// Ping(ctx context.Context) error
-	// Executor() *DbTx
 }
 
-var _ DBTX = (*pgxpool.Pool)(nil)
+var _ DBTX = (*Queries)(nil)
 
-type Queryer interface {
-	Query(ctx context.Context, sql string, arguments ...any) (pgx.Rows, error)
-	QueryContext(ctx context.Context, query string, args ...any) (scan.Rows, error)
-	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
-}
 type Queries struct {
 	pool DBTX
 }
 
 func NewQueries(pool DBTX) *Queries {
 	return &Queries{pool: pool}
-}
-
-type rows struct {
-	pgx.Rows
-}
-
-func (r rows) Close() error {
-	r.Rows.Close()
-	return nil
-}
-
-func (r rows) Columns() ([]string, error) {
-	fields := r.FieldDescriptions()
-	cols := make([]string, len(fields))
-
-	for i, field := range fields {
-		cols[i] = field.Name
-	}
-
-	return cols, nil
-}
-
-func (v *Queries) QueryContext(ctx context.Context, query string, args ...any) (scan.Rows, error) {
-	r, err := v.pool.Query(ctx, query, args...)
-	if err != nil {
-		return nil, err
-	}
-
-	return rows{r}, nil
-}
-
-func (v *Queries) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
-	tag, err := v.pool.Exec(ctx, query, args...)
-	if err != nil {
-		return nil, err
-	}
-
-	return driver.RowsAffected(tag.RowsAffected()), err
 }
 
 func (v *Queries) Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error) {
--- internal/queries/queryer.go
+++ internal/queries/queryer.go
@@ -2,16 +2,14 @@ package queries
 
 import (
 	"context"
-	"database/sql"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/stephenafamo/scan"
 )
 
 type Queryer interface {
-	QueryContext(ctx context.Context, query string, args ...any) (scan.Rows, error)
-	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	// QueryContext(ctx context.Context, query string, args ...any) (scan.Rows, error)
+	// ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	Query(ctx context.Context, sql string, arguments ...any) (pgx.Rows, error)
 	Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
 }
