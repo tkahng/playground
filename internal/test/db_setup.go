0fc24f09eb22d890f123e7cb5c3040e88620b12c
tkahng <tkahng@gmail.com>
2025-05-11 15:58:18 -0700

    dbsetup test

--- internal/test/db_setup.go
+++ internal/test/db_setup.go
@@ -2,15 +2,21 @@ package test
 
 import (
 	"context"
+	"sync"
 
 	"github.com/tkahng/authgo/internal/db"
 )
 
-func DbSetup() (context.Context, *db.Queries) {
-	var (
-		ctx context.Context = context.Background()
-		dbx *db.Queries     = db.CreateQueries(ctx, "postgres://postgres@localhost:5432/authgo_test?sslmode=disable")
-	)
+var (
+	ctxInstance context.Context
+	ctxOnce     sync.Once
+	dbx         *db.Queries
+)
 
-	return ctx, dbx
+func DbSetup() (context.Context, *db.Queries) {
+	ctxOnce.Do(func() {
+		ctxInstance = context.Background()
+		dbx = db.CreateQueries(ctxInstance, "postgres://postgres@localhost:5432/authgo_test?sslmode=disable")
+	})
+	return ctxInstance, dbx
 }
