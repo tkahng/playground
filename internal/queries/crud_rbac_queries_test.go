818bd8bc7af70e886c5304f0f2d3f71ac8039ab6
tkahng <tkahng@gmail.com>
2025-05-12 05:41:30 -0700

    count test

--- internal/queries/crud_rbac_queries_test.go
+++ internal/queries/crud_rbac_queries_test.go
@@ -72,3 +72,200 @@ func TestListPermissions(t *testing.T) {
 		return test.EndTestErr
 	})
 }
+func TestCountPermissions(t *testing.T) {
+	ctx, dbx := test.DbSetup()
+	dbx.RunInTransaction(ctx, func(tx db.Dbx) error {
+		err := queries.EnsureRoleAndPermissions(
+			ctx,
+			tx,
+			shared.PermissionNameAdmin,
+			shared.PermissionNameAdmin,
+			shared.PermissionNameBasic,
+		)
+		if err != nil {
+			t.Fatalf("failed to ensure role and permissions: %v", err)
+		}
+
+		tests := []struct {
+			name    string
+			filter  *shared.PermissionsListFilter
+			want    int64
+			wantErr bool
+		}{
+			{
+				name: "Count all permissions",
+				filter: &shared.PermissionsListFilter{
+					Names: []string{
+						shared.PermissionNameAdmin,
+						shared.PermissionNameBasic,
+					},
+				},
+				want:    2,
+				wantErr: false,
+			},
+			{
+				name: "Count filtered permissions",
+				filter: &shared.PermissionsListFilter{
+					Names: []string{
+						shared.PermissionNameAdmin,
+					},
+				},
+				want:    1,
+				wantErr: false,
+			},
+			{
+				name:    "Count with nil filter",
+				filter:  nil,
+				want:    2,
+				wantErr: false,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := queries.CountPermissions(ctx, tx, tt.filter)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("CountPermissions() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				if got != tt.want {
+					t.Errorf("CountPermissions() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+		return test.EndTestErr
+	})
+}
+func TestListRoles(t *testing.T) {
+	ctx, dbx := test.DbSetup()
+	dbx.RunInTransaction(ctx, func(tx db.Dbx) error {
+		err := queries.EnsureRoleAndPermissions(
+			ctx,
+			tx,
+			shared.PermissionNameAdmin,
+			shared.PermissionNameAdmin,
+			shared.PermissionNameBasic,
+		)
+		if err != nil {
+			t.Fatalf("failed to ensure role and permissions: %v", err)
+		}
+		type args struct {
+			ctx   context.Context
+			db    db.Dbx
+			input *shared.RolesListParams
+		}
+		tests := []struct {
+			name      string
+			args      args
+			wantCount int
+			wantErr   bool
+		}{
+			{
+				name: "Test List Roles",
+				args: args{
+					ctx: ctx,
+					db:  tx,
+					input: &shared.RolesListParams{
+						RoleListFilter: shared.RoleListFilter{
+							Names: []string{
+								shared.PermissionNameAdmin,
+							},
+						},
+						PaginatedInput: shared.PaginatedInput{
+							Page:    0,
+							PerPage: 20,
+						},
+					},
+				},
+				wantCount: 1,
+				wantErr:   false,
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := queries.ListRoles(tt.args.ctx, tt.args.db, tt.args.input)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("ListRoles() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				if !reflect.DeepEqual(len(got), tt.wantCount) {
+					t.Errorf("ListRoles() = %v, want %v", len(got), tt.wantCount)
+				}
+			})
+		}
+		return test.EndTestErr
+	})
+}
+func TestCountRoles(t *testing.T) {
+	ctx, dbx := test.DbSetup()
+	dbx.RunInTransaction(ctx, func(tx db.Dbx) error {
+		err := queries.EnsureRoleAndPermissions(
+			ctx,
+			tx,
+			shared.PermissionNameAdmin,
+			shared.PermissionNameAdmin,
+			shared.PermissionNameBasic,
+		)
+
+		if err != nil {
+			t.Fatalf("failed to ensure role and permissions: %v", err)
+		}
+		err = queries.EnsureRoleAndPermissions(
+			ctx,
+			tx,
+			shared.PermissionNameBasic,
+			shared.PermissionNameBasic,
+		)
+		if err != nil {
+			t.Fatalf("failed to ensure role and permissions: %v", err)
+		}
+		tests := []struct {
+			name    string
+			filter  *shared.RoleListFilter
+			want    int64
+			wantErr bool
+		}{
+			{
+				name: "Count all roles",
+				filter: &shared.RoleListFilter{
+					Names: []string{
+						shared.PermissionNameAdmin,
+						shared.PermissionNameBasic,
+					},
+				},
+				want:    2,
+				wantErr: false,
+			},
+			{
+				name: "Count filtered roles",
+				filter: &shared.RoleListFilter{
+					Names: []string{
+						shared.PermissionNameAdmin,
+					},
+				},
+				want:    1,
+				wantErr: false,
+			},
+			{
+				name:    "Count with nil filter",
+				filter:  nil,
+				want:    2,
+				wantErr: false,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := queries.CountRoles(ctx, tx, tt.filter)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("CountRoles() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				if got != tt.want {
+					t.Errorf("CountRoles() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+		return test.EndTestErr
+	})
+}
