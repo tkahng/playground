0e0a6dbc59359092f518c973fe900d46cb1e4741
tkahng <tkahng@gmail.com>
2025-06-07 06:37:18 -0700

    add types

---
+++ internal/tools/types/omitable_nullable.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+
+	"github.com/danielgtaylor/huma/v2"
+)
+
+// OmittableNullable is a field which can be omitted from the input,
+// set to `null`, or set to a value. Each state is tracked and can
+// be checked for in handling code.
+type OmittableNullable[T any] struct {
+	Sent  bool
+	Null  bool
+	Value T
+}
+
+// UnmarshalJSON unmarshals this value from JSON input.
+func (o *OmittableNullable[T]) UnmarshalJSON(b []byte) error {
+	if len(b) > 0 {
+		o.Sent = true
+		if bytes.Equal(b, []byte("null")) {
+			o.Null = true
+			return nil
+		}
+		return json.Unmarshal(b, &o.Value)
+	}
+	return nil
+}
+
+// Schema returns a schema representing this value on the wire.
+// It returns the schema of the contained type.
+func (o OmittableNullable[T]) Schema(r huma.Registry) *huma.Schema {
+	return r.Schema(reflect.TypeOf(o.Value), true, "")
+}
