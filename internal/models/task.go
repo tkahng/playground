01490ef385da6e032f1763f39740c7fe8aad817d
tkahng <tkahng@gmail.com>
2025-06-14 21:28:37 -0700

    create task stats

--- internal/models/task.go
+++ internal/models/task.go
@@ -65,3 +65,10 @@ const (
 )
 
 type TaskProjectStatus string
+
+type TaskStats struct {
+	TotalProjects     int64 `db:"total_projects" json:"total_projects"`
+	CompletedProjects int64 `db:"completed_projects" json:"completed_projects"`
+	TotalTasks        int64 `db:"total_tasks" json:"total_tasks"`
+	CompletedTasks    int64 `db:"completed_tasks" json:"completed_tasks"`
+}
