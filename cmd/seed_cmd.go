5e15d108bf297af083b83d73fa08da913d550028
tkahng <tkahng@gmail.com>
2025-06-30 13:29:27 -0700

    update seed command

--- cmd/seed_cmd.go
+++ cmd/seed_cmd.go
@@ -1,12 +1,17 @@
 package cmd
 
 import (
+	"errors"
 	"log/slog"
 
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"github.com/spf13/cobra"
 	"github.com/tkahng/authgo/internal/conf"
+	"github.com/tkahng/authgo/internal/core"
 	"github.com/tkahng/authgo/internal/database"
+	"github.com/tkahng/authgo/internal/services"
 	"github.com/tkahng/authgo/internal/stores"
+	"github.com/tkahng/authgo/internal/tools/slug"
 )
 
 func NewSeedCmd() *cobra.Command {
@@ -64,34 +69,76 @@ var seedRolesCmd = &cobra.Command{
 	},
 }
 
-// var seedUserCmd = &cobra.Command{
-// 	Use:   "users",
-// 	Short: "seed users",
-// 	RunE: func(cmd *cobra.Command, args []string) error {
-// 		ctx := cmd.Context()
-// 		conf := conf.GetConfig[conf.DBConfig]()
+var seedUserCmd = &cobra.Command{
+	Use:     "user",
+	Short:   "seed user",
+	Example: "seed user admin@k2dv.io Password123!",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 2 {
+			return errors.New("missing email and password arguments")
+		}
 
-// 		pool := db.CreatePool(ctx, conf)
-// 		dbx := db.NewQueries(pool)
-// 		// role, err := queries.FindRoleByName(ctx, dbx, "basic")
-// 		// if err != nil {
-// 		// 	return fmt.Errorf("error at createing users: %w", err)
-// 		// }
+		if args[0] == "" || is.EmailFormat.Validate(args[0]) != nil {
+			return errors.New("mrror missing or invalid email address")
+		}
+		email := args[0]
+		password := args[0]
 
-// 		// // _, err = seeders.UserCredentialsRolesFactory(ctx, dbx, 20, role)
-// 		// // err := repository.PopulateRoles(ctx, dbx)
-// 		// if err != nil {
+		ctx := cmd.Context()
+		cfg := conf.GetConfig[conf.EnvConfig]()
+		app := core.NewBaseApp(ctx, cfg)
+		params := &services.AuthenticationInput{
+			Email:    email,
+			Password: &password,
+		}
+		_, err := app.Auth().Authenticate(
+			ctx,
+			params,
+		)
+		return err
+	},
+}
 
-// 		// 	return fmt.Errorf("error at createing users: %w", err)
-// 		// }
-// 		// err = seeders.UserOauthFactory(ctx, dbx, 10, models.ProvidersGoogle)
-// 		// if err != nil {
-// 		// 	return fmt.Errorf("error at createing users: %w", err)
-// 		// }
-// 		// err = seeders.UserOauthFactory(ctx, dbx, 10, models.ProvidersGithub)
-// 		// if err != nil {
-// 		// 	return fmt.Errorf("error at createing users: %w", err)
-// 		// }
-// 		return nil
-// 	},
-// }
+var seedTeam = &cobra.Command{
+	Use:     "team",
+	Short:   "seed team",
+	Example: "seed team admin@k2dv.io teamSlug",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 2 {
+			return errors.New("missing email and password arguments")
+		}
+
+		if args[0] == "" || is.EmailFormat.Validate(args[0]) != nil {
+			return errors.New("mrror missing or invalid email address")
+		}
+		email := args[0]
+		slug := slug.NewSlug(args[1])
+
+		ctx := cmd.Context()
+		cfg := conf.GetConfig[conf.EnvConfig]()
+		app := core.NewBaseApp(ctx, cfg)
+		user, err := app.Adapter().User().FindUser(ctx, &stores.UserFilter{
+			Emails: []string{email},
+		})
+		if err != nil {
+			return err
+		}
+		if user == nil {
+			return errors.New("user not found")
+		}
+
+		team, err := app.Team().CreateTeamWithOwner(
+			ctx,
+			slug,
+			slug,
+			user.ID,
+		)
+		if err != nil {
+			return err
+		}
+		if team == nil {
+			return errors.New("team not found")
+		}
+		return err
+	},
+}
