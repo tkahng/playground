5a5d8e73c280141f599235818971bcce8e320048
tkahng <tkahng@gmail.com>
2025-07-06 01:35:07 -0700

    team payments

--- ui/src/pages/payment/payment-success.tsx
+++ ui/src/pages/payment/payment-success.tsx
@@ -1,25 +1,15 @@
 import { NexusAILandingHeader } from "@/components/nexus-landing-header";
 import { NexusAIMinimalFooter } from "@/components/nexus-minimal-footer";
-import { RouteMap } from "@/components/route-map";
 import { Button } from "@/components/ui/button";
 import { useAuthProvider } from "@/hooks/use-auth-provider";
+import { GetError } from "@/lib/get-error";
 import { getCheckoutSession } from "@/lib/queries";
 import { useQuery } from "@tanstack/react-query";
 import { CheckCircle, Home, Settings } from "lucide-react";
 import { Link, useSearchParams } from "react-router";
-// /payment/success?sessionId
 
-// interface PaymentDetails {
-//   confirmationNumber: string;
-//   date: string;
-//   time: string;
-//   plan: string;
-//   nextBillingDate: string;
-//   paymentMethod: string;
-// }
 export default function PaymentSuccessPage() {
   const { user } = useAuthProvider();
-  //   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get("sessionId");
   const { data, isPending, isError, error } = useQuery({
@@ -35,19 +25,22 @@ export default function PaymentSuccessPage() {
       return sub;
     },
   });
-
   if (isPending) {
     return <div>Loading...</div>;
   }
 
   if (isError) {
-    return <div>Error: {error.message}</div>;
+    const err = GetError(error);
+    return <div>Error: {err?.detail}</div>;
   }
 
   if (!data) {
     return <div>No data</div>;
   }
-
+  const team = data?.stripe_customer?.team;
+  if (!team) {
+    return <div>No team</div>;
+  }
   return (
     <div className="flex flex-col min-h-screen ">
       <div className="px-4 md:px-6 lg:px-8 py-2 items-center sticky top-0 z-50 w-full bg-background shadow-sm border-b">
@@ -67,13 +60,13 @@ export default function PaymentSuccessPage() {
 
           <div className="flex flex-col sm:flex-row gap-2">
             <Button variant="outline" className="flex-1" asChild>
-              <Link to={RouteMap.ACCOUNT_OVERVIEW}>
+              <Link to={`teams/${team?.slug}/dashboard`}>
                 <Home className="mr-2 h-4 w-4" />
                 Dashboard
               </Link>
             </Button>
             <Button className="flex-1" asChild>
-              <Link to={RouteMap.BILLING_SETTINGS}>
+              <Link to={`/teams/${team?.slug}/settings/billing`}>
                 <Settings className="mr-2 h-4 w-4" />
                 Manage Subscription
               </Link>
