3ba996854f2db1848225bb78f506dfefed7d76fd
tkahng <tkahng@gmail.com>
2025-07-16 03:37:33 -0700

    add icon

--- ui/src/components/nexus-logo.tsx
+++ ui/src/components/nexus-logo.tsx
@@ -1,10 +1,10 @@
-import { Brain } from "lucide-react";
 import { Link } from "react-router";
+import Icon from "../assets/icon.svg";
 
 export default function PlaygroundLogo() {
   return (
     <Link className="flex items-center justify-center" to={"/"}>
-      <Brain className="h-6 w-6 text-primary" />
+      <img src={Icon} className="h-8 w-8 rounded-sm" />
       <span className="ml-4 text-2xl font-bold text-primary">Playground</span>
     </Link>
   );
