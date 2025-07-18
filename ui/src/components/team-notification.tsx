0ccbd580251a21c51c60c770fb61c1b62ff340e4
tkahng <tkahng@gmail.com>
2025-07-17 23:47:39 -0700

    fix sse

--- ui/src/components/team-notification.tsx
+++ ui/src/components/team-notification.tsx
@@ -1,26 +1,56 @@
 import { useAuthProvider } from "@/hooks/use-auth-provider";
-import { useSSE } from "@/hooks/use-sse";
 import { useTeam } from "@/hooks/use-team";
-import { GetError } from "@/lib/get-error";
 import { TeamMemberNotificationData } from "@/schema.types";
+import {
+  useEventSource,
+  useEventSourceListener,
+} from "@react-nano/use-event-source";
+import { useReducer } from "react";
 
 function TeamNotification() {
   const { user } = useAuthProvider();
   const { teamMember } = useTeam();
-  const { data, error } = useSSE<TeamMemberNotificationData>(
+  function messageReducer(
+    state: TeamMemberNotificationData,
+    action: TeamMemberNotificationData
+  ) {
+    return {
+      ...state,
+      ...action,
+    };
+  }
+  const [stats, updateStats] = useReducer(messageReducer, {
+    notification: {
+      title: "",
+      body: "",
+    },
+    data: {
+      email: "",
+      team_id: "",
+      team_member_id: "",
+    },
+  });
+
+  const [eventSource] = useEventSource(
     "/api/team-members/" +
       teamMember?.id +
       "/notifications/sse?access_token=" +
-      user?.tokens.access_token
+      user?.tokens.access_token,
+    false
+  );
+  useEventSourceListener(
+    eventSource,
+    ["new_team_member"],
+    (evt) => {
+      updateStats(JSON.parse(evt.data)?.user_reaction_stats);
+    },
+    [updateStats]
   );
 
   if (!user) return <p>User not found</p>;
   if (!teamMember) return <p>teamMember not found</p>;
-  if (error) {
-    const err = GetError(error);
-    return <p>Error: {err?.detail}</p>;
-  }
-  return <div> {data?.notification.title}</div>;
+
+  return <div> {stats?.notification.title}</div>;
 }
 
 export default TeamNotification;
--- ui/src/pages/say-hello/say-hello.tsx
+++ ui/src/pages/say-hello/say-hello.tsx
@@ -1,20 +1,53 @@
 import { Button } from "@/components/ui/button";
-import { useSSE } from "@/hooks/use-sse";
+import { Card } from "@/components/ui/card";
 import { userReactionQueries } from "@/lib/api";
+import { UserReaction, UserReactionsStats } from "@/schema.types";
 import {
-  UserReaction,
-  UserReactionsSseMessage,
-  UserReactionsStats,
-} from "@/schema.types";
+  useEventSource,
+  useEventSourceListener,
+} from "@react-nano/use-event-source";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { toast } from "sonner";
 
+type UserReactionsStats2 = UserReactionsStats & {
+  last_reactions: UserReaction[];
+};
+
+// type UserReactionStatsSse = {
+//   user_reaction_stats: UserReactionsStats2;
+// };
 export default function SayHelloPage() {
-  const [stats, setStats] = useState<UserReactionsStats | null>(null);
-  const [latestReactions, setLatestReactions] = useState<UserReaction[]>([]);
-  const { data: sseData, error: sseError } = useSSE<UserReactionsSseMessage>(
-    `/api/user-reactions/sse`
+  // const [stats, setStats] = useState<UserReactionsStats2 | null>(null);
+  function messageReducer(
+    state: UserReactionsStats2,
+    action: UserReactionsStats2
+  ) {
+    const prevLastReactions = state.last_reactions;
+    if (action.last_created) {
+      if (!prevLastReactions.some((r) => r.id === action.last_created?.id)) {
+        prevLastReactions.push(action.last_created);
+      }
+    }
+    return {
+      ...action,
+      last_reactions: prevLastReactions,
+    };
+  }
+  const [stats, updateStats] = useReducer(messageReducer, {
+    top_five_countries: [],
+    total_reactions: 0,
+    last_reactions: [],
+  });
+
+  const [eventSource] = useEventSource("api/user-reactions/sse", false);
+  useEventSourceListener(
+    eventSource,
+    ["latest_user_reaction_stats"],
+    (evt) => {
+      updateStats(JSON.parse(evt.data)?.user_reaction_stats);
+    },
+    [updateStats]
   );
   const { data: statsData, isLoading: isStatsLoading } = useQuery({
     queryKey: ["user-reactions-stats"],
@@ -34,44 +67,17 @@ export default function SayHelloPage() {
     },
   });
   useEffect(() => {
-    if (statsData) {
-      setStats(statsData);
-    }
+    if (statsData) updateStats({ ...statsData, last_reactions: [] });
   }, [statsData]);
-  useEffect(() => {
-    if (sseError) {
-      toast.error("Error", {
-        description: sseError,
-        action: {
-          label: "Close",
-          onClick: () => console.log("Close"),
-        },
-      });
-    } else if (sseData) {
-      if (sseData.event === "latest_user_reaction_stats") {
-        console.log(sseData);
-        toast.success("Success", {
-          description: sseData.event,
-          action: {
-            label: "Close",
-            onClick: () => console.log("Close"),
-          },
-        });
-        setStats(sseData.data.user_reaction_stats);
-        if (sseData.data.user_reaction_stats.last_created) {
-          const latestReaction = sseData.data.user_reaction_stats.last_created;
-          setLatestReactions((prev) => [...[latestReaction], ...prev]);
-        }
-      }
-    }
 
-    return () => {};
-  }, [sseData, sseError]);
   const onClick = () => {
     mutation.mutate();
   };
+  if (isStatsLoading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div>
+    <div className="w-1/2 ml-auto mr-auto">
       <div>SayHelloPage</div>
       <div>
         <Button onClick={onClick}>Say Hello</Button>
@@ -84,8 +90,11 @@ export default function SayHelloPage() {
             <div>Total Reactions: {stats?.total_reactions}</div>
             <div>Latest Reactions: From {stats?.last_created?.city}</div>
             <div>
-              {latestReactions.map((r) => (
-                <div key={r.id}>{r.city}</div>
+              {stats.last_reactions?.map((r) => (
+                <Card key={r.id} className="bg-primary">
+                  <div>{r.city}</div>
+                  <div>{r.created_at}</div>
+                </Card>
               ))}
             </div>
           </>
