b99dcb342ee8126ddadf72f70ef23a9fb6caa06c
tkahng <tkahng@gmail.com>
2025-07-23 14:19:00 -0700

    task edit dropwdown

---
+++ ui/src/pages/teams/projects/tasks/task-edit-dropdown.tsx
@@ -0,0 +1,102 @@
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ConfirmDialog, useDialog } from "@/hooks/use-dialog";
+import { useTeam } from "@/hooks/use-team";
+import { Task } from "@/schema.types";
+import { Ellipsis, Pencil, Trash } from "lucide-react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+export function TaskEditDropDown({
+  task,
+  onDelete,
+}: {
+  taskId: string;
+  task: Task;
+  onDelete: (taskId: string) => void;
+}) {
+  const { team } = useTeam();
+  const editDialog = useDialog();
+  const navigate = useNavigate();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  return (
+    <>
+      <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <Ellipsis className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onSelect={() => {
+              setDropdownOpen(false);
+              navigate(
+                `/teams/${team?.slug}/projects/${task.project_id}/tasks/${task.id}`
+              );
+            }}
+          >
+            <Button variant="ghost" size="sm">
+              <Pencil className="h-4 w-4" />
+              <span>Edit</span>
+            </Button>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onSelect={() => {
+              setDropdownOpen(false);
+              editDialog.trigger();
+            }}
+          >
+            <Button variant="ghost" size="sm">
+              <Trash className="h-4 w-4" />
+              <span>Remove</span>
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <ConfirmDialog dialogProps={editDialog.props}>
+        <>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>This action cannot be undone.</DialogDescription>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button
+                variant="outline"
+                onClick={() => {
+                  console.log("cancel");
+                }}
+              >
+                Cancel
+              </Button>
+            </DialogClose>
+            <DialogClose asChild>
+              <Button
+                variant="destructive"
+                onClick={() => {
+                  console.log("delete");
+                  onDelete(task.id);
+                }}
+              >
+                Delete
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </>
+      </ConfirmDialog>
+    </>
+  );
+}
