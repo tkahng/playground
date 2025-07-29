ca0e9f966b57ab64e2451d295610b38dabdb6012
tkahng <tkahng@gmail.com>
2025-07-28 18:35:27 -0700

    refactor middleware

---
+++ internal/middleware/http_payment_middleware.go
@@ -0,0 +1,150 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/tkahng/playground/internal/contextstore"
+	"github.com/tkahng/playground/internal/core"
+	"github.com/tkahng/playground/internal/models"
+	appHttp "github.com/tkahng/playground/internal/tools/http"
+)
+
+func HttpSelectOrCreateOwnerCustomerFromTeam(app core.App) HttpMiddelwareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rawCtx := r.Context()
+
+			teamInfo := contextstore.GetContextTeamInfo(rawCtx)
+			if teamInfo == nil {
+				_ = appHttp.WriteErr(w, r, http.StatusForbidden, "no team info found")
+				return
+			}
+			customer, err := app.Payment().FindCustomerByTeam(rawCtx, teamInfo.Team.ID)
+			if err != nil {
+				_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error getting customer", err)
+				return
+			}
+			if customer == nil {
+				userInfo := contextstore.GetContextUserInfo(rawCtx)
+				if userInfo == nil {
+					_ = appHttp.WriteErr(w, r, http.StatusForbidden, "no user info found")
+					return
+				}
+				customer, err = app.Payment().CreateTeamCustomer(rawCtx, &teamInfo.Team, &models.User{
+					ID:    userInfo.User.ID,
+					Name:  userInfo.User.Name,
+					Email: userInfo.User.Email,
+				})
+				if err != nil {
+					_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error creating customer", err)
+					return
+				}
+				if customer == nil {
+					_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error creating customer")
+					return
+				}
+				newCtx := contextstore.SetContextCurrentCustomer(rawCtx, customer)
+				r = r.WithContext(newCtx)
+				next.ServeHTTP(w, r)
+				return
+			}
+			newCtx := contextstore.SetContextCurrentCustomer(rawCtx, customer)
+			r = r.WithContext(newCtx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+func HttpSelectCustomerFromTeam(app core.App) HttpMiddelwareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rawCtx := r.Context()
+
+			teamInfo := contextstore.GetContextTeamInfo(rawCtx)
+			if teamInfo == nil {
+				_ = appHttp.WriteErr(w, r, http.StatusForbidden, "no team info found")
+				return
+			}
+			// if teamInfo.Member.Role != models.TeamMemberRoleOwner {
+			// 	_ = appHttp.WriteErr(w, r, http.StatusForbidden, "not a team owner")
+			// 	return
+			// }
+			customer, err := app.Payment().FindCustomerByTeam(rawCtx, teamInfo.Team.ID)
+			if err != nil {
+				_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error getting customer", err)
+				return
+			}
+			if customer == nil {
+				_ = appHttp.WriteErr(w, r, http.StatusUnauthorized, "customer not found")
+				return
+				// userInfo := contextstore.GetContextUserInfo(rawCtx)
+				// if userInfo == nil {
+				// 	_ = appHttp.WriteErr(w, r, http.StatusForbidden, "no user info found")
+				// 	return
+				// }
+				// customer, err = app.Payment().CreateTeamCustomer(rawCtx, &teamInfo.Team, &models.User{
+				// 	ID:    userInfo.User.ID,
+				// 	Name:  userInfo.User.Name,
+				// 	Email: userInfo.User.Email,
+				// })
+				// if err != nil {
+				// 	_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error creating customer", err)
+				// 	return
+				// }
+				// if customer == nil {
+				// 	_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error creating customer")
+				// 	return
+				// }
+				// newCtx := contextstore.SetContextCurrentCustomer(rawCtx, customer)
+				// ctx = r.WithContext(ctx, newCtx)
+				// next(ctx)
+				// return
+			}
+			newCtx := contextstore.SetContextCurrentCustomer(rawCtx, customer)
+			r = r.WithContext(newCtx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func HttpSelectCustomerFromUser(app core.App) HttpMiddelwareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rawCtx := r.Context()
+			userInfo := contextstore.GetContextUserInfo(rawCtx)
+			if userInfo == nil {
+				_ = appHttp.WriteErr(w, r, http.StatusForbidden, "no user info found")
+				return
+			}
+			customer, err := app.Payment().FindCustomerByUser(rawCtx, userInfo.User.ID)
+			if err != nil {
+				_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error getting customer", err)
+				return
+			}
+			if customer == nil {
+				_ = appHttp.WriteErr(w, r, http.StatusUnauthorized, "customer not found")
+				return
+				// customer, err = app.Payment().CreateUserCustomer(rawCtx, &models.User{
+				// 	ID:    userInfo.User.ID,
+				// 	Name:  userInfo.User.Name,
+				// 	Email: userInfo.User.Email,
+				// })
+				// if err != nil {
+				// 	_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error creating customer", err)
+				// 	return
+				// }
+				// if customer == nil {
+				// 	_ = appHttp.WriteErr(w, r, http.StatusInternalServerError, "error creating customer")
+				// 	return
+				// }
+				// newCtx := contextstore.SetContextCurrentCustomer(rawCtx, customer)
+				// ctx = r.WithContext(ctx, newCtx)
+				// next(ctx)
+				// return
+			}
+			newCtx := contextstore.SetContextCurrentCustomer(rawCtx, customer)
+			r = r.WithContext(newCtx)
+			next.ServeHTTP(w, r)
+		})
+	}
+
+}
