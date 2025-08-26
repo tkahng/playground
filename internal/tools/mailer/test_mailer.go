3e77d52d7ef92c64a0ba3fa8b84290a728b0c7d4
tkahng <tkahng@gmail.com>
2025-08-25 21:42:21 -0700

    add messages

--- internal/tools/mailer/test_mailer.go
+++ internal/tools/mailer/test_mailer.go
@@ -3,12 +3,14 @@ package mailer
 import "sync"
 
 type TestMailer struct {
-	Mailer Mailer
-	Wg     *sync.WaitGroup
+	Mailer   Mailer
+	Messages []*Message
+	Wg       *sync.WaitGroup
 }
 
 // Send implements Mailer.
 func (t *TestMailer) Send(message *Message) error {
+	t.Messages = append(t.Messages, message)
 	err := t.Mailer.Send(message)
 	if t.Wg != nil {
 		t.Wg.Done()
