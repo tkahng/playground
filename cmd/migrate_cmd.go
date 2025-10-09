937a3e3eaa8b833de0b58439ff5d4c7490ecbaeb
tkahng <tkahng@gmail.com>
2025-10-09 04:23:55 -0700

    fix migrate cmd

--- cmd/migrate_cmd.go
+++ cmd/migrate_cmd.go
@@ -8,7 +8,6 @@ import (
 )
 
 func NewMigrateCmd() *cobra.Command {
-	migrateCmd.PersistentFlags().Bool("test", false, "is for test?")
 	migrateCmd.AddCommand(upCmd)
 	migrateCmd.AddCommand(resetCmd)
 	migrateCmd.AddCommand(makeSchema)
@@ -21,29 +20,19 @@ var migrateCmd = &cobra.Command{
 	Use:   "migrate",
 	Short: "migrate",
 }
-func migrateUp(cmd *cobra.Command, args []string) error {
-	cfg := conf.GetConfig[conf.DBConfig]()
-	isTest, err := cmd.Flags().GetBool("test")
-	if err != nil {
-		return err
-	}
-	mConfig := database.MigratorConfig{
-		AutoDumpSchema: false,
-	}
-	if isTest {
-		mConfig.DatabaseUrl = cfg.TestDatabaseUrl
-	} else {
-		mConfig.DatabaseUrl = cfg.DatabaseUrl
-	}
-
-	migrator := database.NewMigrator(&mConfig)
-	return migrator.CreateAndMigrate()
-}
 
 var upCmd = &cobra.Command{
 	Use:   "up",
 	Short: "migrate up",
-	RunE: migrateUp,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg := conf.GetConfig[conf.DBConfig]()
+		mConfig := database.MigratorConfig{
+			AutoDumpSchema: false,
+			DatabaseUrl:    cfg.GetUrl(),
+		}
+		migrator := database.NewMigrator(&mConfig)
+		return migrator.CreateAndMigrate()
+	},
 }
 
 var resetCmd = &cobra.Command{
@@ -51,19 +40,10 @@ var resetCmd = &cobra.Command{
 	Short: "migrate reset",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := conf.GetConfig[conf.DBConfig]()
-		isTest, err := cmd.Flags().GetBool("test")
-		if err != nil {
-			return err
-		}
 		mConfig := database.MigratorConfig{
-			AutoDumpSchema:  false,
+			AutoDumpSchema: false,
+			DatabaseUrl:    cfg.GetUrl(),
 		}
-		if isTest {
-			mConfig.DatabaseUrl = cfg.TestDatabaseUrl
-		} else {
-			mConfig.DatabaseUrl = cfg.DatabaseUrl
-		}
-		
 		migrator := database.NewMigrator(&mConfig)
 		return migrator.Reset()
 	},
@@ -73,23 +53,12 @@ var makeSchema = &cobra.Command{
 	Short: "migrate schema",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cfg := conf.GetConfig[conf.DBConfig]()
-		isTest, err := cmd.Flags().GetBool("test")
-		if err != nil {
-			return err
-		}
 		mConfig := database.MigratorConfig{
-			AutoDumpSchema:  true,
+			AutoDumpSchema: false,
+			DatabaseUrl:    cfg.GetUrl(),
 		}
-		if isTest {
-			mConfig.DatabaseUrl = cfg.TestDatabaseUrl
-		} else {
-			mConfig.DatabaseUrl = cfg.DatabaseUrl
-		}
-		
+
 		migrator := database.NewMigrator(&mConfig)
 		return migrator.DumpSchema()
 	},
 }
-
-
-
