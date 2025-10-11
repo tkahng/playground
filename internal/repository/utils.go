f092aec4eb18f1736f113289c269372c56211908
tkahng <tkahng@gmail.com>
2025-10-11 04:17:35 -0700

    add tagOption parser

--- internal/repository/utils.go
+++ internal/repository/utils.go
@@ -4,6 +4,28 @@ import (
 	"strings"
 )
 
+type tagOption struct {
+	Key   string
+	Value string
+}
+
+func parseTagOption(s string) *tagOption {
+	if s == "" {
+		return nil
+	}
+	var option tagOption
+	for idx, item := range strings.Split(s, "=") {
+		if idx == 0 {
+			option.Key = item
+		} else if idx == 1 {
+			option.Value = item
+		} else {
+			break
+		}
+	}
+	return &option
+}
+
 func splitTagValueOptions(tagValue string) (string, []string) {
 	var value string
 	var options []string
