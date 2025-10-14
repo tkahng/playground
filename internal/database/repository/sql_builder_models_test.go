aa1b29926d32a54c227c3786176770bfb4f235eb
tkahng <tkahng@gmail.com>
2025-10-14 09:02:18 -0700

    add more fields to test model

--- internal/database/repository/sql_builder_models_test.go
+++ internal/database/repository/sql_builder_models_test.go
@@ -1,30 +1,38 @@
 package repository_test
 
-import "github.com/tkahng/playground/internal/database/repository"
+import (
+	"time"
+
+	"github.com/tkahng/playground/internal/database/repository"
+)
 
 type A struct {
-	_  struct{} `db:"a" json:"-"`
-	ID string   `db:"id" json:"id"`
-	B  *B       `db:"b" src:"id" dest:"a_id" table:"b" json:"b,omitempty"`
-	Cs []*C     `db:"cs" src:"id" dest:"a_id" table:"c" json:"cs,omitempty"`
-	Ds []*D     `db:"ds" src:"id" dest:"id" table:"d" through:"ad" through_src:"a_id" through_dest:"d_id" json:"ds,omitempty"`
+	_   struct{} `db:"a" json:"-"`
+	ID  string   `db:"id,pk" json:"id"`
+	Age int64    `db:"age" json:"age"`
+	B   *B       `db:"b" src:"id" dest:"a_id" table:"b" json:"b,omitempty"`
+	Cs  []*C     `db:"cs" src:"id" dest:"a_id" table:"c" json:"cs,omitempty"`
+	Ds  []*D     `db:"ds" src:"id" dest:"id" table:"d" through:"ad" through_src:"a_id" through_dest:"d_id" json:"ds,omitempty"`
 }
 type B struct {
-	_   struct{} `db:"public.b" json:"-"`
-	ID  string   `db:"id" json:"id"`
-	AID string   `db:"a_id" json:"a_id"`
-	A   *A       `db:"a" src:"a_id" dest:"id" table:"a" json:"a,omitempty"`
+	_    struct{} `db:"public.b" json:"-"`
+	ID   string   `db:"id" json:"id"`
+	Body string   `db:"body" json:"body"`
+	AID  string   `db:"a_id" json:"a_id"`
+	A    *A       `db:"a" src:"a_id" dest:"id" table:"a" json:"a,omitempty"`
 }
 type C struct {
-	_   struct{} `db:"public.b" json:"-"`
-	ID  string   `db:"id" json:"id"`
-	AID string   `db:"a_id" json:"a_id"`
-	A   *A       `db:"a" src:"a_id" dest:"id" table:"a" json:"a,omitempty"`
+	_    struct{} `db:"public.b" json:"-"`
+	ID   string   `db:"id" json:"id"`
+	Code int64    `db:"code" json:"code"`
+	AID  string   `db:"a_id" json:"a_id"`
+	A    *A       `db:"a" src:"a_id" dest:"id" table:"a" json:"a,omitempty"`
 }
 type D struct {
-	_  struct{} `db:"public.b" json:"-"`
-	ID string   `db:"id" json:"id"`
-	As []*A     `db:"as" src:"id" dest:"id" table:"a" through:"ad" through_src:"d_id" through_dest:"a_id" json:"as,omitempty"`
+	_    struct{}  `db:"public.b" json:"-"`
+	ID   string    `db:"id" json:"id"`
+	Date time.Time `db:"date" json:"date"`
+	As   []*A      `db:"as" src:"id" dest:"id" table:"a" through:"ad" through_src:"d_id" through_dest:"a_id" json:"as,omitempty"`
 }
 type AD struct {
 	_   struct{} `db:"public.b" json:"-"`
