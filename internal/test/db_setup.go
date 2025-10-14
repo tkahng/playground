0caada7e943167f9578402b281b2cbfd7b9c201f
tkahng <tkahng@gmail.com>
2025-10-14 15:11:02 -0700

    update dbsetup

--- internal/test/db_setup.go
+++ internal/test/db_setup.go
@@ -6,7 +6,12 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/tkahng/playground/internal/conf"
 	"github.com/tkahng/playground/internal/database"
+)
+
+const (
+	TestDbUrl = "postgres://postgres:postgres@localhost:5432/playground_test?sslmode=disable"
 )
 
 var (
@@ -16,12 +21,12 @@ var (
 	dbx         *database.Queries
 )
 
-func SingletonDbSetupTest(t *testing.T) (context.Context, *database.Queries) {
+func SingletonDbSetup(t *testing.T) (context.Context, *database.Queries) {
 	t.Helper()
-	return dbSetup()
+	return singletonDbSetup()
 }
 
-func dbSetup() (context.Context, *database.Queries) {
+func singletonDbSetup() (context.Context, *database.Queries) {
 	ctxOnce.Do(func() {
 		ctxInstance = context.Background()
 		dbx = database.CreateSingletonQueriesContext(ctxInstance, "postgres://postgres:postgres@localhost:5432/playground_test?sslmode=disable")
@@ -31,11 +36,40 @@ func dbSetup() (context.Context, *database.Queries) {
 
 func WithSingletonTx(t *testing.T, fn func(ctx context.Context, db database.Dbx)) {
 	t.Helper()
-	ctx, dbx := dbSetup()
+	ctx, dbx := singletonDbSetup()
 	tx, err := dbx.Begin(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
+	// nolint:errcheck
+	defer tx.Rollback(ctx)
+	// panic handle
+	defer func() {
+		if recErr := recover(); recErr != nil {
+			err := tx.Rollback(ctx)
+			if err != nil {
+				t.Error(err)
+			}
+			t.Fatal(recErr)
+		}
+	}()
+	fn(ctx, database.NewTxQueries(tx))
+}
+
+// hello
+
+func WithTx(t *testing.T, cfg *conf.EnvConfig, fn func(ctx context.Context, db database.Dbx)) {
+	t.Helper()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	dbx := database.CreateQueriesContext(ctx, cfg.Db.GetDatabaseUrl())
+	tx, err := dbx.Begin(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		dbx.Close()
+	}()
 	// nolint:errcheck
 	defer tx.Rollback(ctx)
 	// panic handle
