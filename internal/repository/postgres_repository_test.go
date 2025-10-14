a3bf9a440fba510a3d5b43b9454b2f7ffc34fb0d
tkahng <tkahng@gmail.com>
2025-10-14 04:20:36 -0700

    remove test

--- internal/repository/postgres_repository_test.go
+++ internal/repository/postgres_repository_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/tkahng/playground/internal/database"
 	"github.com/tkahng/playground/internal/models"
 	"github.com/tkahng/playground/internal/test"
-	"github.com/tkahng/playground/internal/tools/types"
 )
 
 func UserCompareFunc(got, want models.User) bool {
@@ -28,37 +27,7 @@ func UserCompareFunc(got, want models.User) bool {
 }
 
 func TestRepository(t *testing.T) {
-	t.Run("test post", func(t *testing.T) {
-		test.WithTx(t, func(ctx context.Context, db database.Dbx) {
-			record := models.User{
-				Name:  types.Pointer("Test User"),
-				Email: "test@example.com",
-			}
+	test.WithTx(t, func(ctx context.Context, db database.Dbx) {
 
-			users, err := User.Post(ctx, db, []models.User{record})
-			if err != nil || users == nil {
-				t.Fatalf("Failed to create user: %v", err)
-			}
-			if len(users) != 1 {
-				t.Fatalf("Expected 1 user, got %d", len(users))
-			}
-			user := users[0]
-			if UserCompareFunc(*user, record) {
-				t.Fatalf("Expected user to be different, got %v", user)
-			}
-		})
-	})
-	t.Run("test post exec", func(t *testing.T) {
-		test.WithTx(t, func(ctx context.Context, db database.Dbx) {
-			record := models.User{
-				Name:  types.Pointer("Test User"),
-				Email: "test@example.com",
-			}
-
-			users, err := User.PostExec(ctx, db, []models.User{record})
-			if err != nil || users != 1 {
-				t.Fatalf("Failed to create user: %v", err)
-			}
-		})
 	})
 }
