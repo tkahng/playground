722b773147a905b6b1fea6eda4ad1de0dcb77959
tkahng <tkahng@gmail.com>
2025-10-14 05:13:59 -0700

    update test

--- internal/database/repository/postgres_repository_helpers_test.go
+++ internal/database/repository/postgres_repository_helpers_test.go
@@ -12,7 +12,9 @@ import (
 func TestInitRbac(t *testing.T) {
 	t.Parallel()
 	test.WithTx(t, func(ctx context.Context, dbx database.Dbx) {
+		// init
 		repository.InitRbac(t, dbx)
+		// find all
 		roles := repository.MustFindAll(t, repository.Role, dbx, nil)
 		if len(roles) != 4 {
 			t.Errorf("expected 4 roles, got %d", len(roles))
@@ -20,6 +22,52 @@ func TestInitRbac(t *testing.T) {
 		permissions := repository.MustFindAll(t, repository.Permission, dbx, nil)
 		if len(permissions) != 4 {
 			t.Errorf("expected 4 permissions, got %d", len(permissions))
+		}
+		// find roles with permissions
+		rolesWithPermissions := repository.MustFindAll(t, repository.Role, dbx, &map[string]any{
+			"permissions": map[string]any{
+				"name": map[string]any{
+					"_eq": "admin",
+				},
+			},
+		})
+		if len(rolesWithPermissions) != 1 && rolesWithPermissions[0].Name != "admin" {
+			t.Errorf("expected 1 roles with permissions, got %d", len(rolesWithPermissions))
+		}
+		rolesWithPermissions = repository.MustFindAll(t, repository.Role, dbx, &map[string]any{
+			"permissions": map[string]any{
+				"name": map[string]any{
+					"_eq": "basic",
+				},
+			},
+		})
+		if len(rolesWithPermissions) != 4 {
+			t.Errorf("expected 4 roles with permissions, got %d", len(rolesWithPermissions))
+		}
+		rolesWithPermissions = repository.MustFindAll(t, repository.Role, dbx, &map[string]any{
+			"_or": []map[string]any{
+				{
+					"permissions": map[string]any{
+						"name": map[string]any{
+							"_eq": "admin",
+						},
+					},
+				},
+				{
+					"_and": []map[string]any{
+						{
+							"permissions": map[string]any{
+								"name": map[string]any{
+									"_eq": "advanced",
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+		if len(rolesWithPermissions) != 2 {
+			t.Errorf("expected 2 roles with permissions, got %d", len(rolesWithPermissions))
 		}
 	})
 }
