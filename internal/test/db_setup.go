4496a53332ecc78ab5b2a27b4c87f0d6fbe6bbe8
tkahng <tkahng@gmail.com>
2025-10-15 12:50:13 -0700

    cleanup test dbx

--- internal/test/db_setup.go
+++ internal/test/db_setup.go
@@ -34,27 +34,6 @@ func singletonDbSetup(url string) (context.Context, *database.Queries) {
 	return ctxInstance, dbx
 }
 
-func WithSingletonTx(t *testing.T, fn func(ctx context.Context, db database.Dbx)) {
-	ctx, dbx := singletonDbSetup(TestDbUrl)
-	tx, err := dbx.Begin(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// nolint:errcheck
-	defer tx.Rollback(ctx)
-	// panic handle
-	defer func() {
-		if recErr := recover(); recErr != nil {
-			err := tx.Rollback(ctx)
-			if err != nil {
-				t.Error(err)
-			}
-			t.Fatal(recErr)
-		}
-	}()
-	fn(ctx, database.NewTxQueries(tx))
-}
-
 // WithNewTx creates a new pool connection, runs the test within that transaciton, rolls back, and closes the pool.
 func WithNewTx(t *testing.T, fn func(ctx context.Context, db database.Dbx)) {
 	t.Helper()
