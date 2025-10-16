e2d5c09128ed75f9bc8ae05ea6e9b3b63126d30a
tkahng <tkahng@gmail.com>
2025-10-15 17:59:56 -0700

    add messaging

--- internal/database/migrations/20250404060015_add notification table.sql
+++ internal/database/migrations/20250404060015_add notification table.sql
@@ -1,34 +1,27 @@
 -- migrate:up
--- Create "notifications" table
-CREATE TABLE "notifications" (
-    "id" uuid NOT NULL DEFAULT uuidv7(),
-    "created_at" timestamptz not null default clock_timestamp(),
-    "updated_at" timestamptz not null default clock_timestamp(),
-    "read_at" timestamptz,
-    "channel" text not null,
-    "payload" jsonb not null,
-    "user_id" uuid,
-    "team_member_id" uuid,
-    "team_id" uuid,
-    "metadata" jsonb not null default '{}',
-    "type" text not null,
-    PRIMARY KEY ("id"),
-    CONSTRAINT "fk_notifications_user" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT "fk_notifications_team_member" FOREIGN KEY ("team_member_id") REFERENCES "team_members" ("id") ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT "fk_notifications_team" FOREIGN KEY ("team_id") REFERENCES "teams" ("id") ON UPDATE CASCADE ON DELETE CASCADE
+-- Create messaging.notifications table
+CREATE TABLE IF NOT EXISTS messaging.notifications (
+    id uuid NOT NULL DEFAULT uuidv7(),
+    created_at timestamptz not null default clock_timestamp(),
+    updated_at timestamptz not null default clock_timestamp(),
+    read_at timestamptz,
+    channel text not null,
+    payload jsonb not null default '{}'::jsonb,
+    user_id uuid,
+    team_member_id uuid,
+    team_id uuid,
+    metadata jsonb not null default '{}'::jsonb,
+    type text not null,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_notifications_user FOREIGN KEY (user_id) REFERENCES auth.users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_notifications_team_member FOREIGN KEY (team_member_id) REFERENCES org.team_members (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_notifications_team FOREIGN KEY (team_id) REFERENCES org.teams (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
--- Add function "notify_after_notification_insert"
-CREATE OR REPLACE FUNCTION notify_after_notification_insert() RETURNS TRIGGER AS $BODY$ BEGIN PERFORM pg_notify('notification', row_to_json(NEW)::text);
-PERFORM pg_notify(NEW.channel, row_to_json(NEW)::text);
-RETURN NEW;
-END;
-$BODY$ LANGUAGE plpgsql;
--- Add trigger "trigger_notify_after_notification_insert"
-CREATE OR REPLACE TRIGGER trigger_notify_after_notification_insert
-AFTER
-INSERT ON notifications FOR EACH ROW EXECUTE PROCEDURE notify_after_notification_insert();
+-- Create trigger
+CREATE TRIGGER handle_messaging_notifications_updated_at before
+update on billing.stripe_products for each row execute procedure utility.set_current_timestamp_updated_at();
 -- migrate:down
-DROP TRIGGER IF EXISTS trigger_notify_after_notification_insert ON notifications;
-DROP FUNCTION IF EXISTS notify_after_notification_insert;
-DROP TABLE IF EXISTS notifications;
-DROP TYPE IF EXISTS notification_type;
+-- Drop trigger
+DROP TRIGGER IF EXISTS handle_messaging_notifications_updated_at ON messaging.notifications;
+-- Drop table
+DROP TABLE IF EXISTS messaging.notifications;
