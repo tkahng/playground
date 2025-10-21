86d0c51d60b662e6a9c82b017bad95701c28d81e
tkahng <tkahng@gmail.com>
2025-10-20 17:49:16 -0700

    whereerror2

--- internal/database/repository/sql_builder.go
+++ internal/database/repository/sql_builder.go
@@ -552,6 +552,188 @@ func (b *SQLBuilder[Model]) getSortedFields(where *map[string]any) []*fieldIdx {
 	}
 	return fields
 }
+
+func (b *SQLBuilder[Model]) Where2(where *map[string]any, args *[]any) string {
+	if where == nil {
+		return ""
+	}
+
+	// Check for special conditions
+	// _not, _and, and _or are used for logical operations
+	if item, ok := (*where)["_not"]; ok {
+		// in case of _not, the value is a map[string]any
+		expr, ok := item.(map[string]any)
+		if ok {
+			return "NOT (" + b.Where(&expr, args) + ")"
+		}
+	} else if items, ok := (*where)["_and"]; ok {
+		// in case of _and, the value is a []map[string]any
+		result := []string{}
+		ands, ok := items.([]map[string]any)
+		if ok {
+			for _, item := range ands {
+				expr := item
+				result = append(result, b.Where(&expr, args))
+			}
+		}
+
+		return "(" + strings.Join(result, " AND ") + ")"
+	} else if ors, ok := (*where)["_or"]; ok {
+		// in case of _and, the value is a []map[string]any
+		result := []string{}
+		orWheres, ok := ors.([]map[string]any)
+		if ok {
+			for _, item := range orWheres {
+				expr := item
+				result = append(result, b.Where(&expr, args))
+			}
+		}
+
+		return "(" + strings.Join(result, " OR ") + ")"
+	}
+
+	// Otherwise, construct the WHERE clause based on the field names and operations
+	result := []string{}
+
+	fieldIdxs := b.getSortedFields(where)
+	for _, fieldIdx := range fieldIdxs {
+		var whereFieldName = fieldIdx.Name
+		if whereFieldValue, ok := (*where)[fieldIdx.Name]; ok {
+			expr, ok := whereFieldValue.(map[string]any)
+			if ok {
+				for whereOp, whereOpValue := range expr {
+					// if this field and operation is registered, go ahead.
+					// if not, it might be a relational field
+					if opFunc, ok := b.operations[whereFieldName+whereOp]; ok {
+						var whereField = b.GetFieldByName(whereFieldName)
+						if whereField == nil {
+							panic(fmt.Sprintf("could not get field by name: field %s not found for model %s", whereFieldName, b.tableName))
+						}
+						// Primitive field condition detected
+						//
+						// if the value is nil, it should use the _isNil, _isNotNil operations
+						if whereOpValue == nil {
+							if slices.Contains(nilOps, whereOp) {
+								// If the value is nil and the operation is a nil operation, send it
+								result = append(result, opFunc(whereField.ColumnName))
+							} else {
+								// If the value is nil and the operation is not a nil operation, ignore it
+								panic("nil value for non-nil operation " + whereOp)
+							}
+							continue
+						}
+
+						_value := reflect.ValueOf(whereOpValue)
+
+						// If the value is a pointer, dereference it
+						if _value.Kind() == reflect.Pointer && !_value.IsNil() {
+							_value = _value.Elem()
+						}
+
+						if !_value.IsValid() {
+							panic(fmt.Sprintf("value is invalid for field %s and operation %s for model %s", whereFieldName, whereOp, b.TableName()))
+						}
+						// String values are passed
+						// time values are formatted as strings
+						// fmt.Stringer values are called cast then called String method
+						if (_value.Kind() == reflect.Slice || _value.Kind() == reflect.Array) && _value.Type().Elem().Kind() != reflect.Uint8 {
+							// Slice or array values are iterated and check for string types.
+							items := []string{}
+							for i := range _value.Len() {
+								_valueItem := _value.Index(i)
+								item := convert(_valueItem)
+								items = append(items, GenerateParameterPlaceholder(reflect.ValueOf(item), args))
+							}
+							result = append(result, opFunc(whereField.QualifiedColumnName, items...))
+
+						} else {
+							item := convert(_value)
+							result = append(result, opFunc(whereField.QualifiedColumnName, GenerateParameterPlaceholder(reflect.ValueOf(item), args)))
+						}
+
+					} else {
+						// this field name and opation is not registered.
+						// Relation field condition detected
+						if relation := b.GetRelationByName(whereFieldName); relation != nil {
+							var relatedBuilder SQLBuilderInterface
+							// Get the target SQLBuilder for the relation
+							if bld, ok := registry[relation.table]; !ok {
+								panic(fmt.Sprintf("relation %s not found for model %s", relation.table, b.tableName))
+							} else {
+								// Get the target SQLBuilder for the relation
+								relatedBuilder = bld
+							}
+
+							// Construct the sub-query for the related table
+							relationWhere := expr
+
+							// query is the subquery we need to generate.
+							var query string
+
+							var relatedDestField = relatedBuilder.GetFieldByName(relation.dest)
+							if relatedDestField == nil {
+								panic(fmt.Sprintf("could not get field by name: dest field %s not found for model %s for relation to %s", relation.dest, relatedBuilder.TableName(), b.tableName))
+							}
+							var srcField = b.GetFieldByName(relation.src)
+							if srcField == nil {
+								panic(fmt.Sprintf("could not get field by name: src field %s not found for model %s", relation.src, b.tableName))
+							}
+							// if through is not empty
+							// it is a many-to-many relation
+							if relation.through != "" {
+								var throughBuilder SQLBuilderInterface
+								// Get the target SQLBuilder for the relation
+								if bld, ok := registry[relation.through]; !ok {
+									panic(fmt.Sprintf("relation through %s not found for model %s", relation.through, b.tableName))
+								} else {
+									// Get the target SQLBuilder for the relation
+									throughBuilder = bld
+								}
+
+								var throughTableName = throughBuilder.TableName()
+								var throughSrcField = throughBuilder.GetFieldByName(relation.throughSrc)
+								if throughSrcField == nil {
+									panic(fmt.Sprintf("could not get field by name: through src field %s not found for model %s for relation to %s", relation.dest, throughBuilder.TableName(), b.tableName))
+								}
+								var throughDestField = throughBuilder.GetFieldByName(relation.throughDest)
+								if throughDestField == nil {
+									panic(fmt.Sprintf("could not get field by name: dest field %s not found for model %s for relation to %s", relation.dest, relatedBuilder.TableName(), relatedBuilder.TableName()))
+								}
+								query = fmt.Sprintf(
+									`SELECT %s FROM %s join %s on %s.%s = %s.%s`,
+									// SELECT
+									throughSrcField.QualifiedColumnName,
+									// FROM
+									throughTableName,
+									// join
+									relatedBuilder.TableName(),
+									// on
+									relatedBuilder.TableName(),
+									relatedDestField.ColumnName,
+									// =
+									throughTableName,
+									throughDestField.ColumnName,
+								)
+							} else {
+
+								query = fmt.Sprintf("SELECT %s FROM %s", relatedDestField.QualifiedColumnName, relatedBuilder.TableName())
+							}
+							if expr := relatedBuilder.Where(&relationWhere, args); expr != "" {
+								query += fmt.Sprintf(" WHERE %s", expr)
+							}
+							if inop, ok := b.operations[srcField.Name+"_in"]; ok {
+								result = append(result, inop(srcField.QualifiedColumnName, query))
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return strings.Join(result, " AND ")
+}
+
 func (b *SQLBuilder[Model]) Where(where *map[string]any, args *[]any) string {
 	if where == nil {
 		return ""
