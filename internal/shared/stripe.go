71bd424a6079706332ee427bb856ae9459d86863
tkahng <tkahng@gmail.com>
2025-10-20 23:46:59 -0700

    remove basic product

--- internal/shared/stripe.go
+++ internal/shared/stripe.go
@@ -1,13 +1,11 @@
 package shared
 
 const (
-	StripeProductBasicID    string = "prod_basic"
 	StripeProductProID      string = "prod_pro"
 	StripeProductAdvancedID string = "prod_advanced"
 )
 
 var StripeProductPermissionMap = map[string]string{
-	StripeProductBasicID:    PermissionNameBasic,
 	StripeProductProID:      PermissionNamePro,
 	StripeProductAdvancedID: PermissionNameAdvanced,
 }
