e15eb9ff957f6d1d08c937a4af666cf1c40e7c83
tkahng <tkahng@gmail.com>
2025-10-21 17:07:36 -0700

    add know rolenames

--- internal/shared/permission.go
+++ internal/shared/permission.go
@@ -6,3 +6,13 @@ const (
 	PermissionNamePro      string = "pro"
 	PermissionNameAdvanced string = "advanced"
 )
+
+var (
+	KnownRoleNames, KnwonPermissionNames                     = []string{PermissionNameAdmin, PermissionNameAdvanced, PermissionNamePro, PermissionNameBasic}, []string{PermissionNameAdmin, PermissionNameAdvanced, PermissionNamePro, PermissionNameBasic}
+	KnownRoleNamesPermissionsMap         map[string][]string = map[string][]string{
+		PermissionNameBasic:    {PermissionNameBasic},
+		PermissionNamePro:      {PermissionNameBasic, PermissionNamePro},
+		PermissionNameAdvanced: {PermissionNameBasic, PermissionNamePro, PermissionNameAdvanced},
+		PermissionNameAdmin:    {PermissionNameBasic, PermissionNamePro, PermissionNameAdvanced, PermissionNameAdmin},
+	}
+)
