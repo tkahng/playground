0eb244bac08c0843d71844bf8b344c72d1aff37f
tkahng <tkahng@gmail.com>
2025-10-26 05:19:25 -0700

    bind route api

--- internal/apis/auth_signout.go
+++ internal/apis/auth_signout.go
@@ -3,10 +3,33 @@ package apis
 import (
 	"context"
 	"fmt"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/tkahng/playground/internal/shared"
 )
 
 type SignoutDto struct {
 	RefreshToken string `json:"refresh_token" cookie:"refresh_token" form:"refresh_token" required:"true"`
+}
+
+func (a *Api) bindSignout(api huma.API) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "signout",
+			Method:      http.MethodPost,
+			Path:        "/auth/signout",
+			Summary:     "Signout",
+			Description: "Signout",
+			Tags:        []string{"Auth"},
+			Errors:      []int{http.StatusUnauthorized, http.StatusNotFound},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+		},
+		a.Signout,
+	)
 }
 
 func (api *Api) Signout(ctx context.Context, input *struct{ Body SignoutDto }) (*struct{}, error) {
