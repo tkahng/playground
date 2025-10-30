46cd1a1f5d761ddd3ac9930e2b30841efeb1880c
tkahng <tkahng@gmail.com>
2025-10-30 08:38:34 -0700

    update signup redirect

--- ui/src/pages/auth/signup.tsx
+++ ui/src/pages/auth/signup.tsx
@@ -30,17 +30,22 @@ export default function SignupPage() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { signUp } = useAuthProvider();
+
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const token = params.get("token");
   const redirectTo = params.get("redirect_to");
   const email = params.get("email");
-  const navigateTo =
-    email && token
-      ? `/team-invitation`
-      : redirectTo
-      ? redirectTo
-      : "/account/dashboard";
+
+  let navigateTo: string;
+  if (email?.length && token?.length) {
+    navigateTo = `/team-invitation`;
+  } else if (redirectTo?.length) {
+    navigateTo = redirectTo;
+    params.delete("redirect_to");
+  } else {
+    navigateTo = "/account/dashboard";
+  }
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setLoading(true);
@@ -52,7 +57,7 @@ export default function SignupPage() {
         name: input.name,
       });
       setLoading(false);
-      navigate({ pathname: navigateTo, search: search });
+      navigate({ pathname: navigateTo, search: params.toString() });
     } catch (error) {
       if (error instanceof Error) {
         toast.error(error.message, {
