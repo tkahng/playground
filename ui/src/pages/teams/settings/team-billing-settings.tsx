0a5bdc66dbd8e94995fad508baf0fde7c29acd60
tkahng <tkahng@gmail.com>
2025-10-30 11:19:03 -0700

    update pricing navigation

--- ui/src/pages/teams/settings/team-billing-settings.tsx
+++ ui/src/pages/teams/settings/team-billing-settings.tsx
@@ -9,7 +9,7 @@ import { useQuery } from "@tanstack/react-query";
 export default function TeamBillingSettingPage() {
   const { user } = useAuthProvider();
   const { team } = useTeam();
-  const { data, isPending } = useQuery({
+  const { data, isPending, isError, error } = useQuery({
     queryKey: ["team-billing-settings"],
     queryFn: async () => {
       if (!user?.tokens.access_token) {
@@ -29,9 +29,9 @@ export default function TeamBillingSettingPage() {
   if (isPending) {
     return <div>Loading...</div>;
   }
-  // if (isError) {
-  //   return <div>Error: {error.message}</div>;
-  // }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
   if (!team?.id) {
     return <div>Error: Team ID is required</div>;
   }
