be662aafb08b1e5ed36cee8aa7acd8f31790075d
tkahng <tkahng@gmail.com>
2025-10-30 00:45:22 -0700

    team select from pricing

---
+++ ui/src/components/pricing/pricing-team.tsx
@@ -0,0 +1,157 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+import { ProductWithPrices, SubscriptionWithPrice, User } from "@/schema.types";
+
+import { useState } from "react";
+import { Link } from "react-router";
+import { z } from "zod";
+
+interface Props {
+  user: User | null | undefined;
+  products: ProductWithPrices[];
+  subscription: SubscriptionWithPrice | null;
+}
+
+type BillingInterval = "lifetime" | "year" | "month";
+
+export const formSchema = z.object({
+  price_id: z.string().min(2, {
+    message: "name must be at least 2 characters.",
+  }),
+});
+
+export default function PricingTeam({ products, subscription }: Props) {
+  // const { user } = useAuthProvider();
+  // const { team, teamMember } = useTeam();
+  const intervals = Array.from(
+    new Set(
+      products.flatMap((product) =>
+        product?.prices?.map((price) => price?.interval)
+      )
+    )
+  );
+
+  const [billingInterval, setBillingInterval] =
+    useState<BillingInterval>("month");
+
+  if (!products.length) {
+    return (
+      <section className="">
+        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:flex-col sm:align-center"></div>
+          <p className="text-4xl font-extrabold sm:text-center sm:text-6xl">
+            No subscription pricing plans found. Create them in your{" "}
+            <a
+              className="text-pink-500 underline"
+              href="https://dashboard.stripe.com/products"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Stripe Dashboard
+            </a>
+            .
+          </p>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:flex-col sm:align-center">
+          <h1 className="text-4xl font-extrabold text-primary sm:text-center sm:text-6xl">
+            Pricing Plans
+          </h1>
+          <p className="max-w-2xl m-auto mt-5 text-xl text-secondary-foreground sm:text-center sm:text-2xl">
+            Start building for free, then add a site plan to go live. Account
+            plans unlock additional features.
+          </p>
+          <div className="relative self-center mt-6 bg-primary-foreground rounded-lg p-0.5 flex sm:mt-8 border">
+            {intervals.includes("month") && (
+              <button
+                onClick={() => setBillingInterval("month")}
+                type="button"
+                className={`${
+                  billingInterval === "month"
+                    ? "relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white"
+                    : "ml-0.5 relative w-1/2 border border-transparent text-zinc-400"
+                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+              >
+                Monthly billing
+              </button>
+            )}
+            {intervals.includes("year") && (
+              <button
+                onClick={() => setBillingInterval("year")}
+                type="button"
+                className={`${
+                  billingInterval === "year"
+                    ? "relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white"
+                    : "ml-0.5 relative w-1/2 border border-transparent text-zinc-400"
+                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+              >
+                Yearly billing
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="mt-12 space-y-0 sm:mt-16 flex flex-wrap justify-center gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0">
+          {products.map((product) => {
+            const price = product?.prices?.find(
+              (price) => price.interval === billingInterval
+            );
+            if (!price) return null;
+            const priceString = new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: price.currency!,
+              minimumFractionDigits: 0,
+            }).format((price?.unit_amount || 0) / 100);
+            return (
+              <div
+                key={product.id}
+                className={cn(
+                  "flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600",
+                  {
+                    "border border-pink-500": subscription
+                      ? product.name === subscription?.price?.product?.name
+                      : product.name === "Freelancer",
+                  },
+                  "flex-1", // This makes the flex item grow to fill the space
+                  "basis-1/3", // Assuming you want each card to take up roughly a third of the container's width
+                  "max-w-xs" // Sets a maximum width to the cards to prevent them from getting too large
+                )}
+              >
+                <div className="p-6">
+                  <h2 className="text-2xl font-semibold leading-6 ">
+                    {product.name}
+                  </h2>
+                  <p className="mt-4 text-muted-foreground">
+                    {product.description}
+                  </p>
+                  <p className="mt-8">
+                    <span className="text-5xl font-extrabold">
+                      {priceString}
+                    </span>
+                    <span className="text-base font-medium">
+                      /{billingInterval}
+                    </span>
+                  </p>
+                  <Button
+                    asChild
+                    className="block w-full py-2 mt-8 text-sm font-semibold text-center rounded-md"
+                  >
+                    <Link to="/teams/settings/billing">
+                      {subscription ? "Manage" : "Subscribe"}
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ ui/src/components/team-select-dialog.tsx
@@ -0,0 +1,171 @@
+import { CreateTeamDialog } from "@/components/create-team-dialog";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useDialog } from "@/hooks/use-dialog";
+import { useUserTeams } from "@/hooks/use-user-teams";
+import { Team } from "@/schema.types";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { JSX, useState } from "react";
+import { Link } from "react-router";
+
+export function TeamSelectDialog({ button }: { button: JSX.Element }) {
+  const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
+  const { data, error: teamsError, isLoading: teamsLoading } = useUserTeams();
+  if (teamsLoading) {
+    return <div>Loading...</div>;
+  }
+  if (teamsError) {
+    return <div>Error: {teamsError?.message}</div>;
+  }
+  if (!data) {
+    return <div>No teams available.</div>;
+  }
+  function handleSelectTeam(team: Team) {
+    setSelectedTeam(team);
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {button}
+        {/* <Button variant="outline">Open Dialog</Button> */}
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Upgrade Team</DialogTitle>
+          <DialogDescription>
+            Choose a team to upgrade to. You can only choose teams where you are
+            a owner.
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="grid gap-4">
+          <TeamSelect onTeamSelect={handleSelectTeam} />
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button asChild disabled={!selectedTeam}>
+            <Link to={`/teams/${selectedTeam?.slug}/settings/billing`}>
+              Continue
+            </Link>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export type TeamSelectProps = {
+  onTeamSelect: (team: Team) => void;
+};
+export function TeamSelect({ onTeamSelect }: TeamSelectProps) {
+  // const navigate = useNavigate();
+  const { props } = useDialog();
+  const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
+  const { data, error: teamsError, isLoading: teamsLoading } = useUserTeams();
+
+  if (teamsLoading) {
+    return <div>Loading...</div>;
+  }
+  if (teamsError) {
+    return <div>Error: {teamsError?.message}</div>;
+  }
+  if (!data) {
+    return <div>No teams available.</div>;
+  }
+  function handleSelectTeam(team: Team) {
+    setSelectedTeam(team);
+    onTeamSelect(team);
+    props.onOpenChange(false);
+  }
+  return (
+    <div className="ml-6">
+      <Popover open={props.open} onOpenChange={props.onOpenChange}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={props.open}
+            aria-label="Select a team"
+            className="w-[200px] justify-between"
+          >
+            {selectedTeam ? selectedTeam.name : "Select a team"}
+            <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandList>
+              <CommandInput placeholder="Select team..." />
+              <CommandEmpty>No team found.</CommandEmpty>
+              <CommandGroup heading="Teams">
+                {data.data.map((te) => (
+                  <CommandItem
+                    key={te.id}
+                    onSelect={() => {
+                      handleSelectTeam(te);
+                    }}
+                    disabled={te.member?.role !== "owner"}
+                    className="text-sm"
+                  >
+                    <Avatar className="mr-2 h-5 w-5">
+                      <AvatarFallback>
+                        {te.name.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex-1">
+                      <div className="font-medium">{te.name}</div>
+                      {/* <div className="text-xs text-muted-foreground">
+                        {team.plan} • {team.role}
+                      </div> */}
+                    </div>
+                    <Check
+                      className={`ml-auto h-4 w-4 ${
+                        te?.id === selectedTeam?.id
+                          ? "opacity-100"
+                          : "opacity-0"
+                      }`}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+            <CommandSeparator />
+            <CommandList>
+              <CommandGroup>
+                <CommandItem>
+                  <CreateTeamDialog />
+                </CommandItem>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
