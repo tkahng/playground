fe2586ab8ebe30c3cb38b43b7d18d1e4df763223
tkahng <tkahng@gmail.com>
2025-11-12 10:29:12 -0800

    add update task test

--- internal/apis/team_task_test.go
+++ internal/apis/team_task_test.go
@@ -11,8 +11,11 @@ import (
 	"github.com/tkahng/playground/internal/apis"
 	"github.com/tkahng/playground/internal/core"
 	"github.com/tkahng/playground/internal/database"
+	"github.com/tkahng/playground/internal/database/repository"
 	"github.com/tkahng/playground/internal/models"
+	"github.com/tkahng/playground/internal/stores"
 	"github.com/tkahng/playground/internal/test"
+	"github.com/tkahng/playground/internal/tools/types"
 )
 
 func TestApi_TeamTaskList(t *testing.T) {
@@ -70,4 +73,55 @@ func TestApi_TeamTaskList(t *testing.T) {
 			tt.Test(t)
 		}
 	})
+}
+
+func TestApi_TeamTaskUpdate(t *testing.T) {
+	tests := []ApiScenario{
+		{
+			Name:           "success: update task",
+			Method:         http.MethodPut,
+			URL:            "/tasks/{task-id}",
+			ExpectedStatus: http.StatusNoContent,
+			BeforeTestFunc: func(t testing.TB, app *core.BaseApp, scenario *ApiScenario) {
+				owner := core.CreateUserWithOptions(t, app, core.UserWithVerifiedNow())
+				team1 := core.CreateTeamAndMemberWithOptions(t, app, &owner.User)
+				project := core.CreateProjectAndTasks(t, app, &team1.Member)
+				task := repository.MustCreateOneCtx(t, t.Context(), repository.Task, app.Db(), &models.Task{
+					TeamID:    team1.Team.ID,
+					ProjectID: project.ID,
+					Status:    models.TaskStatusTodo,
+					Rank:      0,
+				})
+				tokenHeader, _ := core.CreateAccessHeaderAndRefreshToken(t, app, team1.User.Email)
+				scenario.Headers = []string{tokenHeader}
+				scenario.URL = fmt.Sprintf("/tasks/%s", task.ID.String())
+				input := stores.UpdateTaskDto{
+					Name:        "updated",
+					Description: types.Pointer("UpdateTaskDto"),
+					Status:      models.TaskStatusInProgress,
+				}
+				scenario.Store.Set("input", input)
+				scenario.Body = JsonToReader(t, input)
+			},
+			AfterTestFunc: func(t testing.TB, app *core.BaseApp, scenario *ApiScenario, res *httptest.ResponseRecorder) {
+				result := repository.MustFindOneCtx(t, t.Context(), repository.Task, app.Db(), nil)
+				input, ok := scenario.Store.Get("input").(stores.UpdateTaskDto)
+				if !ok {
+					t.Fatal("no input")
+				}
+				assert.Equal(t, input.Name, result.Name)
+				assert.Equal(t, input.Description, result.Description)
+				assert.Equal(t, input.Status, result.Status)
+			},
+		},
+	}
+	for _, tt := range tests {
+		database.WithNewTestTx(t, func(ctx context.Context, db database.Dbx) {
+			testApi := SetupApi(t, ctx, db)
+			tt.TestAppFactory = func(t testing.TB) *TestApi {
+				return testApi
+			}
+			tt.Test(t)
+		})
+	}
 }
