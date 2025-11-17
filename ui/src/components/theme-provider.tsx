9f207b9293c940d63e30657b31ca567e74fb287a
tkahng <tkahng@gmail.com>
2025-11-17 00:21:42 -0800

    feat: theme icons and providers

--- ui/src/components/theme-provider.tsx
+++ ui/src/components/theme-provider.tsx
@@ -3,7 +3,7 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import * as React from "react";
 
-export function ThemeProviderNext({
+export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
--- ui/src/pages/landing/landing.tsx
+++ ui/src/pages/landing/landing.tsx
@@ -16,7 +16,7 @@ export default function Landing() {
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                Welcome to the Playground
+                Welcome to my Playground
               </h1>
               <p className="mx-auto max-w-[700px]">
                 This is a place of learning and experimentation.
