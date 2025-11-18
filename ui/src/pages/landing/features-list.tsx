4ea65731aa0d1d954c328b53a91a1abfb387aaa3
tkahng <tkahng@gmail.com>
2025-11-17 18:35:54 -0800

    add hash router

--- ui/src/pages/landing/features-list.tsx
+++ ui/src/pages/landing/features-list.tsx
@@ -1,4 +1,3 @@
-import { LandingFeatureCardProps } from "@/pages/landing/landing";
 import {
   Banknote,
   Hand,
@@ -7,51 +6,85 @@ import {
   ShieldUser,
   Users,
 } from "lucide-react";
-
-export const landingFeatures: LandingFeatureCardProps[] = [
+export type Feature = {
+  title: string;
+  shortContent: string[];
+  mainContent: string[];
+  icon: React.JSX.Element;
+  fragment: string;
+  path: string;
+  detailLink?: string;
+};
+export const features: Feature[] = [
   {
     title: "Say Hello!",
     icon: <Hand className="h-10 w-10" />,
-    content: ["Whoever you are, wherever you are, you can say hello!"],
-    featureLink: "/features#say-hello",
+    shortContent: ["Whoever you are, wherever you are, you can say hello!"],
+    mainContent: [
+      `This is a simple feature that allows you to say hello to the world. It's a great way to test out the basic functionality of the website and get a feel for how things work.`,
+    ],
+    fragment: "#say-hello",
+    path: "/features",
   },
   {
     title: "Teams",
     icon: <Users className="h-10 w-10" />,
-    content: [
+    shortContent: [
       "Create your own team with members. Easily manage member's roles and access",
     ],
-    featureLink: "/features#teams",
+    mainContent: [
+      `Create and manage your own teams, invite members, and assign roles with ease. Foster collaboration and streamline your workflow by working together on projects.`,
+    ],
+    fragment: "#teams",
+    path: "/features",
   },
   {
     title: "Payment Integration",
     icon: <Banknote className="h-10 w-10" />,
-    content: [
+    shortContent: [
       "Teams can subscribe to different plans and manage their subscriptions.",
     ],
-    featureLink: "/features#payment-integration",
+    fragment: "#payment-integration",
+    path: "/features",
+    mainContent: [
+      `Integrate with Stripe to manage subscriptions and products. Offer various plans to your teams and handle billing with confidence.`,
+    ],
   },
   {
     title: "Projects & Tasks",
     icon: <ListTodo className="h-10 w-10" />,
-    content: [
+    shortContent: [
       "Tackle real-world problems by creating projects with tasks. Assign others and track progress.",
     ],
-    featureLink: "/features#projects-and-tasks",
+    fragment: "#projects-and-tasks",
+    path: "/features",
+    mainContent: [
+      `Break down your work into manageable projects and tasks. Assign tasks to team members, set deadlines, and monitor progress to ensure timely completion.`,
+    ],
   },
   {
     title: "Authentication",
     icon: <IdCard className="h-10 w-10" />,
-    content: ["Join your way: From Email/Password to Google, Github, etc."],
-    featureLink: "/features#authentication",
+    shortContent: [
+      "Join your way: From Email/Password to Google, Github, etc.",
+    ],
+    mainContent: [
+      `Experience seamless and secure login with various authentication options, including email/password, Google, and GitHub. Your data is protected with the latest security protocols.`,
+    ],
+    fragment: "#authentication",
+    path: "/features",
   },
 
   {
     title: "Admin",
     icon: <ShieldUser className="h-10 w-10" />,
-    content: [
+    shortContent: [
       "Manage users, roles and permissions, products, and subscriptions",
     ],
-    featureLink: "/features#admin",
+    fragment: "#admin",
+    path: "/features",
+    mainContent: [
+      `Gain full control over your platform with robust admin features. Manage users, roles, permissions, products, and subscriptions from a centralized dashboard.`,
+    ],
   },
 ];
