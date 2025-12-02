f3099b12d6013f28722b31d95b621a0b5634b2f0
tkahng <tkahng@gmail.com>
2025-12-02 02:35:41 -0800

    feat(store): update gaming store with participant impl

--- internal/stores/gaming_test_helpers.go
+++ internal/stores/gaming_test_helpers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/tkahng/playground/internal/models"
@@ -35,7 +36,7 @@ func WithUserID(userID uuid.UUID) PlayerOptionFunc {
 	}
 }
 
-func MustCreatePlayer(t testing.TB, ctx context.Context, gamingStore *DBGamingStore, fns ...PlayerOptionFunc) *models.Player {
+func MustCreatePlayer(t testing.TB, ctx context.Context, gamingStore GamingStore, fns ...PlayerOptionFunc) *models.Player {
 	key := security.RandomString(16)
 	player := &models.Player{
 		Email:    fmt.Sprintf("%s@example", key),
@@ -73,7 +74,7 @@ func WithStatus(status models.FriendshipStatus) FriendshipOptionFunc {
 		opt.Status = status
 	}
 }
-func MustCreateFriendship(t testing.TB, ctx context.Context, gamingStore *DBGamingStore, player1, player2 *models.Player, fns ...FriendshipOptionFunc) *models.Frindship {
+func MustCreateFriendship(t testing.TB, ctx context.Context, gamingStore GamingStore, player1, player2 *models.Player, fns ...FriendshipOptionFunc) *models.Frindship {
 	friendship := &models.Frindship{
 		InvitedPlayerID:    player1.ID,
 		RequestingPlayerID: player2.ID,
@@ -96,4 +97,102 @@ func MustCreateFriendship(t testing.TB, ctx context.Context, gamingStore *DBGami
 		t.Errorf("CreateFriendship() = %v, want requesting player id %v", newFriendship, player2.ID)
 	}
 	return newFriendship
+}
+
+type RpsGameOptionFunc func(opt *models.RpsGame)
+
+func WithRpsGameStatus(status models.RpsGameStatus) RpsGameOptionFunc {
+	return func(opt *models.RpsGame) {
+		opt.Status = status
+	}
+}
+
+func WithRpsGameExpiresAt(expiresAt time.Time) RpsGameOptionFunc {
+	return func(opt *models.RpsGame) {
+		opt.ExpiresAt = expiresAt
+	}
+}
+
+func WithRpsGameMetadata(metadata string) RpsGameOptionFunc {
+	return func(opt *models.RpsGame) {
+		opt.Metadata = []byte(metadata)
+	}
+}
+
+func WithRpsGameParticipants(participants []*models.RpsParticipant) RpsGameOptionFunc {
+	return func(opt *models.RpsGame) {
+		opt.Participants = participants
+	}
+}
+
+func MustCreateRpsGame(t testing.TB, gamingStore GamingStore, fns ...RpsGameOptionFunc) *models.RpsGame {
+	key := security.RandomString(16)
+	game := &models.RpsGame{
+		Status:    models.RpsGameStatusPending,
+		ExpiresAt: time.Now().UTC().Add(time.Hour * 36),
+		Metadata:  []byte(fmt.Sprintf(`{"key": "%s"}`, key)),
+	}
+	for _, fn := range fns {
+		fn(game)
+	}
+	newGame, err := gamingStore.CreateRpsGame(t.Context(), game)
+	if err != nil {
+		t.Fatalf("CreateRpsGame() error = %v", err)
+	}
+	return newGame
+}
+
+type RpsParticipantOptionFunc func(opt *models.RpsParticipant)
+
+func WithRpsParticipantStatus(status models.RpsParticipantStatus) RpsParticipantOptionFunc {
+	return func(opt *models.RpsParticipant) {
+		opt.Status = status
+	}
+}
+
+func WithRpsParticipantMove(move models.RpsParticipantMove) RpsParticipantOptionFunc {
+	return func(opt *models.RpsParticipant) {
+		opt.Move = move
+	}
+}
+
+func WithRpsParticipantResult(result models.RpsParticipantResult) RpsParticipantOptionFunc {
+	return func(opt *models.RpsParticipant) {
+		opt.Result = result
+	}
+}
+
+func WithRpsParticipantMetadata(metadata string) RpsParticipantOptionFunc {
+	return func(opt *models.RpsParticipant) {
+		opt.Metadata = []byte(metadata)
+	}
+}
+
+func WithRpsParticipantType(typ models.RpsParticipantType) RpsParticipantOptionFunc {
+	return func(opt *models.RpsParticipant) {
+		opt.Type = typ
+	}
+}
+
+func WithRpsParticipantRespondedAt(repondedAt time.Time) RpsParticipantOptionFunc {
+	return func(opt *models.RpsParticipant) {
+		opt.RespondedAt = &repondedAt
+	}
+}
+
+func MustCreateRpsParticipant(t testing.TB, gamingStore GamingStore, fns ...RpsParticipantOptionFunc) *models.RpsParticipant {
+	participant := &models.RpsParticipant{
+		Type:   models.RpsParticipantTypeHost,
+		Status: models.RpsParticipantStatusPending,
+		Move:   models.RpsParticipantMovePaper,
+		Result: models.RpsParticipantResultTie,
+	}
+	for _, fn := range fns {
+		fn(participant)
+	}
+	newParticipant, err := gamingStore.CreateRpsParticipant(t.Context(), participant)
+	if err != nil {
+		t.Fatalf("CreateRpsParticipant() error = %v", err)
+	}
+	return newParticipant
 }
