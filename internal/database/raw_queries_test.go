85f377f26839837fa2957b8c37f2713def66f241
tkahng <tkahng@gmail.com>
2025-12-03 03:20:26 -0800

    feat(database): add template based isolated persistence for testing

---
+++ internal/database/raw_queries_test.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/tkahng/playground/internal/conf"
+	"github.com/tkahng/playground/internal/tools/security"
+)
+
+func TestCreateDatabaseWithTemplate(t *testing.T) {
+	cfg := conf.ZeroEnvConfig()
+	cfg.Db.Db = "postgres"
+	dbx, err := CreatePool(context.Background(), cfg.Db.GetDatabaseUrl())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dbx.Close()
+	name := fmt.Sprintf("playground_test_%s", security.RandomString(10))
+	err = CreateDatabaseWithTemplate(context.Background(), dbx, name, "playground_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = DeleteDatabase(context.Background(), dbx, name)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
