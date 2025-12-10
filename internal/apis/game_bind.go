12712c004fdb8c50d282f4684c9c61649e98f57b
tkahng <tkahng@gmail.com>
2025-12-10 09:08:29 -0800

    feat(api): add player middleware

--- internal/apis/game_bind.go
+++ internal/apis/game_bind.go
@@ -1,6 +1,20 @@
 package apis
 
+import (
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/tkahng/playground/internal/middleware"
+	"github.com/tkahng/playground/internal/middleware/humamiddleware"
+)
+
 func bindGameApi(api *Api) {
-	bindGetUserPlayerApi(api)
-	bindPutUserPlayerApi(api)
+	gameGroup := huma.NewGroup(api.Api())
+	gameGroup.UseMiddleware(
+		humamiddleware.HumaChiMiddlewares(
+			middleware.SetCurrentPlayerMiddleware(api.App()),
+		)...,
+	)
+	bindGetMyPlayerApi(gameGroup, api.App())
+	bindPutMyPlayerApi(gameGroup, api.App())
+	bindFindPlayersApi(gameGroup, api.App())
+	bindFindRegisteredPlayerByEmailApi(gameGroup, api.App())
 }
