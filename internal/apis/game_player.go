b29c2d8779f1fccc2ea8ff377e995fd5d8646b5d
tkahng <tkahng@gmail.com>
2025-12-11 14:57:18 -0800

    feat(api): refactor game api files

---
+++ internal/apis/game_player.go
@@ -0,0 +1,221 @@
+package apis
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/tkahng/playground/internal/contextstore"
+	"github.com/tkahng/playground/internal/core"
+	"github.com/tkahng/playground/internal/middleware"
+	"github.com/tkahng/playground/internal/middleware/humamiddleware"
+	"github.com/tkahng/playground/internal/models"
+	"github.com/tkahng/playground/internal/shared"
+	"github.com/tkahng/playground/internal/stores"
+	"github.com/tkahng/playground/internal/tools/mapper"
+
+	"github.com/tkahng/playground/internal/tools/types"
+	"github.com/tkahng/playground/internal/tools/utils"
+)
+
+func bindGetMyPlayerApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "get-my-player",
+			Method:      http.MethodGet,
+			Path:        "/players/me",
+			Summary:     "get my player.",
+			Description: "Gets a player for the user. Returns the player if there is one.",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+		},
+		func(ctx context.Context, input *struct{}) (*ApiSingleOutput[*Player], error) {
+			user := contextstore.GetContextUserInfo(ctx)
+			if user == nil {
+				return nil, huma.Error401Unauthorized("Unauthorized. No user info")
+			}
+			player, err := app.Adapter().Gaming().FindPlayer(ctx, &stores.PlayersFilter{
+				Emails: []string{user.User.Email},
+			})
+			if err != nil {
+				return nil, err
+			}
+			return &ApiSingleOutput[*Player]{
+				Body: ApiSingleResponse[*Player]{
+					Data: ToApiPlayer(player),
+				},
+			}, nil
+		},
+	)
+}
+
+type GamePutPlayerMeArgs struct {
+	DisplayName *string `json:"display_name" required:"true" nullable:"true" minLength:"1" maxLength:"80"`
+}
+
+func bindPutMyPlayerApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "put-my-player",
+			Method:      http.MethodPut,
+			Path:        "/players/me",
+			Summary:     "Put user player.",
+			Description: "Creates a player for the user if there is none, otherwise updates the player. Returns the player.",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+		},
+		func(ctx context.Context, input *struct {
+			Body GamePutPlayerMeArgs
+		}) (*ApiSingleOutput[*Player], error) {
+			user := contextstore.GetContextUserInfo(ctx)
+			if user == nil {
+				return nil, huma.Error401Unauthorized("Unauthorized. No user info")
+			}
+			player, err := app.Adapter().Gaming().FindPlayer(ctx, &stores.PlayersFilter{
+				Emails: []string{user.User.Email},
+			})
+			if err != nil {
+				return nil, err
+			}
+			if player == nil {
+				player, err = app.Adapter().Gaming().CreatePlayer(ctx, &models.Player{
+					Email:       user.User.Email,
+					UserID:      &user.User.ID,
+					DisplayName: input.Body.DisplayName,
+				})
+				if err != nil {
+					return nil, err
+				}
+			} else {
+				player, err = app.Adapter().Gaming().UpdatePlayer(ctx, &models.Player{
+					ID:          player.ID,
+					UserID:      &user.User.ID,
+					DisplayName: user.User.Name,
+					Email:       user.User.Email,
+					Metadata:    player.Metadata,
+				})
+				if err != nil {
+					return nil, err
+				}
+			}
+			return &ApiSingleOutput[*Player]{
+				Body: ApiSingleResponse[*Player]{
+					Data: ToApiPlayer(player),
+				},
+			}, nil
+		},
+	)
+}
+
+type PlayersFilter struct {
+	PaginatedInput
+	SortParams
+	Ids          []string                  `query:"ids,omitempty" required:"false" minimum:"1" maximum:"100" format:"uuid"`
+	Q            string                    `query:"q,omitempty" required:"false"`
+	Emails       []string                  `query:"emails,omitempty" required:"false" minimum:"1" maximum:"100" format:"email"`
+	DisplayNames []string                  `query:"display_names,omitempty" required:"false" minimum:"1" maximum:"100" format:"email"`
+	UserIds      []string                  `query:"user_ids,omitempty" required:"false" minimum:"1" maximum:"100" format:"uuid"`
+	Registered   types.OptionalParam[bool] `query:"registered,omitempty" required:"false"`
+}
+
+func bindFindPlayersApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "get-players",
+			Method:      http.MethodGet,
+			Path:        "/players",
+			Summary:     "get players.",
+			Description: "gets a list of players.",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+		},
+		func(ctx context.Context, input *PlayersFilter) (*ApiPaginatedOutput[*Player], error) {
+			user := contextstore.GetContextUserInfo(ctx)
+			if user == nil {
+				return nil, huma.Error401Unauthorized("Unauthorized. No user info")
+			}
+			filter := &stores.PlayersFilter{}
+			filter.DisplayNames = input.DisplayNames
+			filter.Emails = input.Emails
+			filter.UserIds = utils.ParseValidUUIDs(input.UserIds...)
+			filter.Registered = input.Registered
+			filter.Ids = utils.ParseValidUUIDs(input.Ids...)
+			filter.Q = input.Q
+			filter.Page = input.Page
+			filter.PerPage = input.PerPage
+			filter.SortBy = input.SortBy
+			filter.SortOrder = input.SortOrder
+			players, err := app.Adapter().Gaming().FindPlayers(ctx, filter)
+			if err != nil {
+				return nil, err
+			}
+			count, err := app.Adapter().Gaming().CountPlayers(ctx, filter)
+			if err != nil {
+				return nil, err
+			}
+			return &ApiPaginatedOutput[*Player]{
+				Body: ApiPaginatedResponse[*Player]{
+					Data: mapper.Map(players, ToApiPlayer),
+					Meta: ApiGenerateMeta(&input.PaginatedInput, count),
+				},
+			}, nil
+		},
+	)
+}
+func bindFindRegisteredPlayerByEmailApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "search-registered-player",
+			Method:      http.MethodGet,
+			Path:        "/players/registered/email/{inviting-player-email}",
+			Summary:     "search registered player by email",
+			Description: "search registered player by email",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+			Middlewares: humamiddleware.HumaChiMiddlewares(
+				middleware.RequireCurrentPlayerMiddelware(),
+			),
+		},
+		func(ctx context.Context, input *struct {
+			Email string `path:"inviting-player-email" required:"true" format:"email"`
+		}) (*ApiSingleOutput[*Player], error) {
+			user := contextstore.GetContextUserInfo(ctx)
+			if user == nil {
+				return nil, huma.Error401Unauthorized("Unauthorized. No user info")
+			}
+			filter := &stores.PlayersFilter{}
+			filter.Emails = []string{input.Email}
+			filter.Registered = types.OptionalParam[bool]{
+				Value: true,
+				IsSet: true,
+			}
+			filter.Page = 0
+			filter.PerPage = 1
+			player, err := app.Adapter().Gaming().FindPlayer(ctx, filter)
+			if err != nil {
+				return nil, err
+			}
+			return &ApiSingleOutput[*Player]{
+				Body: ApiSingleResponse[*Player]{
+					Data: ToApiPlayer(player),
+				},
+			}, nil
+		},
+	)
+}
---
+++ internal/apis/game_rps.go
@@ -0,0 +1,278 @@
+package apis
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/google/uuid"
+	"github.com/tkahng/playground/internal/contextstore"
+	"github.com/tkahng/playground/internal/core"
+	"github.com/tkahng/playground/internal/middleware"
+	"github.com/tkahng/playground/internal/middleware/humamiddleware"
+	"github.com/tkahng/playground/internal/models"
+	"github.com/tkahng/playground/internal/services"
+	"github.com/tkahng/playground/internal/shared"
+	"github.com/tkahng/playground/internal/stores"
+	"github.com/tkahng/playground/internal/tools/security"
+	"github.com/tkahng/playground/internal/tools/types"
+	"github.com/tkahng/playground/internal/workers"
+)
+
+type SubmitMoveToGameInput struct {
+	Move   RpsParticipantMove `json:"move" required:"true" enum:"rock,paper,scissors"`
+	Status RpsGameStatus      `json:"status" required:"true" enum:"pending,completed,cancelled"`
+}
+
+func bindSubmitMoveToRpsGameApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "submit-move-to-rps-game",
+			Method:      http.MethodPost,
+			Path:        "/games/rps/{game-id}/submit-move",
+			Summary:     "submit move to rps game",
+			Description: "submit move to rps game",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+			Middlewares: humamiddleware.HumaChiMiddlewares(
+				middleware.RequireCurrentPlayerMiddelware(),
+			),
+		},
+		func(ctx context.Context, input *struct {
+			GameID string `path:"game-id" required:"true" format:"uuid"`
+			Body   SubmitMoveToGameInput
+		}) (*ApiSingleOutput[*RpsGameWithParticipants], error) {
+			currentPlayer := contextstore.GetContextCurrentPlayer(ctx)
+			if currentPlayer == nil {
+				return nil, huma.Error401Unauthorized("no player found.")
+			}
+			gameId, err := uuid.Parse(input.GameID)
+			if err != nil {
+				return nil, huma.Error400BadRequest("error parsing game id")
+			}
+			game, err := app.RpsGame().FindRpsGameWithParticipants(ctx, gameId)
+			if err != nil {
+				return nil, err
+			}
+			if game == nil {
+				return nil, huma.Error404NotFound("game not found")
+			}
+			if currentPlayer.ID != game.InvitedParticipant.PlayerID {
+				return nil, huma.Error401Unauthorized("not invited player")
+			}
+			var rpsGameWithParticipants *services.RpsGameWithParticipants
+			txErr := app.Adapter().RunInTxCtx(ctx, func(txCtx context.Context) error {
+				rpsGameWithParticipants, err = app.RpsGame().RespondToGameRequest(txCtx, &services.GameRequestResponse{
+					GameID:          game.RpsGame.ID,
+					InvitedPlayerID: currentPlayer.ID,
+					Move:            models.RpsParticipantMove(input.Body.Move),
+					Status:          models.RpsGameStatus(input.Body.Status),
+				})
+				return err
+			})
+			if txErr != nil {
+				return nil, txErr
+			}
+			return &ApiSingleOutput[*RpsGameWithParticipants]{
+				Body: ApiSingleResponse[*RpsGameWithParticipants]{
+					Data: &RpsGameWithParticipants{
+						RpsGame:               toApiRpsGame(rpsGameWithParticipants.RpsGame),
+						RequestingParticipant: ToApiRpsParticipant(rpsGameWithParticipants.RequestingParticipant),
+						InvitedParticipant:    ToApiRpsParticipant(rpsGameWithParticipants.InvitedParticipant),
+					},
+				},
+			}, nil
+		},
+	)
+}
+
+type UnregisteredPlayerInput struct {
+	InvitingPlayerEmail string             `json:"inviting-player-email" required:"true" format:"email"`
+	Move                RpsParticipantMove `json:"move" required:"true" enum:"rock,paper,scissors"`
+}
+
+func bindSendGameRequestToUnRegisteredPlayerApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "send-game-request-to-unregistered-player",
+			Method:      http.MethodPost,
+			Path:        "/games/rps/requests/unregistered",
+			Summary:     "send game request to unregistered player",
+			Description: "send game request to unregistered player",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+			Middlewares: humamiddleware.HumaChiMiddlewares(
+				middleware.RequireCurrentPlayerMiddelware(),
+			),
+		},
+		func(ctx context.Context, input *struct {
+			Body UnregisteredPlayerInput
+		}) (*ApiSingleOutput[*RpsGameWithParticipants], error) {
+			// get current player
+			currentPlayer := contextstore.GetContextCurrentPlayer(ctx)
+			if currentPlayer == nil {
+				return nil, huma.Error401Unauthorized("no player found.")
+			}
+			// find inviting player by email and unregistered.
+			player, err := app.Adapter().Gaming().FindPlayer(ctx, &stores.PlayersFilter{
+				Emails: []string{input.Body.InvitingPlayerEmail},
+				Registered: types.OptionalParam[bool]{
+					Value: false,
+					IsSet: true,
+				},
+			})
+			if err != nil {
+				return nil, err
+			}
+			if player == nil {
+				// if player not found, create player
+				player, err = app.Adapter().Gaming().CreatePlayer(ctx, &models.Player{
+					Email: input.Body.InvitingPlayerEmail,
+				})
+				if err != nil {
+					return nil, err
+				}
+			}
+			// check if player can play again inviting player
+			canPlay, err := app.RpsGame().PlayerCanPlayWithPlayer(ctx, currentPlayer.ID, player.ID)
+			if err != nil {
+				return nil, err
+			}
+			if !canPlay {
+				return nil, huma.Error400BadRequest("player can't play with invited player")
+			}
+			//
+			var rpsGameWithParticipants *services.RpsGameWithParticipants
+			txErr := app.Adapter().RunInTxCtx(ctx, func(txCtx context.Context) error {
+				// request game. returns game and participants.
+				rpsGameWithParticipants, err = app.RpsGame().RequestGame(txCtx, &services.RpsGameRequestInput{
+					RequestingPlayerID:   currentPlayer.ID,
+					InvitedPlayerID:      player.ID,
+					RequestingPlayerMove: models.RpsParticipantMove(input.Body.Move),
+					DurationSeconds:      3 * 24 * 60 * 60,
+				})
+				if err != nil {
+					return err
+				}
+				// create invite
+				invitation, err := app.Adapter().Gaming().CreateRpsGameInvite(txCtx, &models.RpsGameInvite{
+					GameID:             rpsGameWithParticipants.RpsGame.ID,
+					RequestingPlayerID: currentPlayer.ID,
+					InvitedPlayerID:    rpsGameWithParticipants.InvitedParticipant.PlayerID,
+					Token:              security.GenerateTokenKey(),
+				})
+				if err != nil {
+					return err
+				}
+				// send invitation
+				err = app.JobService().EnqueueRpsGameInviteJob(ctx, &workers.RpsGameInvitationJobArgs{
+					Email:          player.Email,
+					InvitedByEmail: currentPlayer.Email,
+					TokenHash:      invitation.Token,
+				})
+				return err
+			})
+			if txErr != nil {
+				return nil, txErr
+			}
+			return &ApiSingleOutput[*RpsGameWithParticipants]{
+				Body: ApiSingleResponse[*RpsGameWithParticipants]{
+					Data: &RpsGameWithParticipants{
+						RpsGame:               toApiRpsGame(rpsGameWithParticipants.RpsGame),
+						RequestingParticipant: ToApiRpsParticipant(rpsGameWithParticipants.RequestingParticipant),
+						InvitedParticipant:    ToApiRpsParticipant(rpsGameWithParticipants.InvitedParticipant),
+					},
+				},
+			}, nil
+		},
+	)
+}
+
+type RpsGameRequestInput struct {
+	InvitingPlayerId uuid.UUID          `json:"inviting_player_id" required:"true" format:"uuid"`
+	Move             RpsParticipantMove `json:"move" required:"true" enum:"rock,paper,scissors"`
+}
+
+func bindSendGameRequestToRegisteredPlayerApi(api huma.API, app core.App) {
+	huma.Register(
+		api,
+		huma.Operation{
+			OperationID: "send-game-request-to-registered-player",
+			Method:      http.MethodPost,
+			Path:        "/games/rps/requests",
+			Summary:     "send game request to registered player",
+			Description: "send game request to registered player",
+			Tags:        []string{"Games", "Player"},
+			Errors:      []int{http.StatusUnauthorized},
+			Security: []map[string][]string{{
+				shared.BearerAuthSecurityKey: {},
+			}},
+			Middlewares: humamiddleware.HumaChiMiddlewares(
+				middleware.RequireCurrentPlayerMiddelware(),
+			),
+		},
+		func(ctx context.Context, input *struct {
+			Body RpsGameRequestInput
+		}) (*ApiSingleOutput[*RpsGameWithParticipants], error) {
+			user := contextstore.GetContextUserInfo(ctx)
+			if user == nil {
+				return nil, huma.Error401Unauthorized("Unauthorized. No user info")
+			}
+			currentPlayer := contextstore.GetContextCurrentPlayer(ctx)
+			if currentPlayer == nil {
+				return nil, huma.Error401Unauthorized("no player found.")
+			}
+			filter := &stores.PlayersFilter{}
+			filter.Ids = []uuid.UUID{input.Body.InvitingPlayerId}
+			filter.Registered = types.OptionalParam[bool]{
+				Value: true,
+				IsSet: true,
+			}
+			player, err := app.Adapter().Gaming().FindPlayer(ctx, filter)
+			if err != nil {
+				return nil, err
+			}
+			if player == nil {
+				return nil, huma.Error404NotFound("player not found")
+			}
+			// check if player can play again inviting player
+			canPlay, err := app.RpsGame().PlayerCanPlayWithPlayer(ctx, currentPlayer.ID, player.ID)
+			if err != nil {
+				return nil, err
+			}
+			if !canPlay {
+				return nil, huma.Error400BadRequest("player can't play with invited player")
+			}
+			var rpsGameWithParticipants *services.RpsGameWithParticipants
+			txErr := app.Adapter().RunInTxCtx(ctx, func(txCtx context.Context) error {
+				rpsGameWithParticipants, err = app.RpsGame().RequestGame(txCtx, &services.RpsGameRequestInput{
+					RequestingPlayerID:   currentPlayer.ID,
+					InvitedPlayerID:      player.ID,
+					RequestingPlayerMove: models.RpsParticipantMove(input.Body.Move),
+					DurationSeconds:      3 * 24 * 60 * 60,
+				})
+				return err
+			})
+			if txErr != nil {
+				return nil, txErr
+			}
+			return &ApiSingleOutput[*RpsGameWithParticipants]{
+				Body: ApiSingleResponse[*RpsGameWithParticipants]{
+					Data: &RpsGameWithParticipants{
+						RpsGame:               toApiRpsGame(rpsGameWithParticipants.RpsGame),
+						RequestingParticipant: ToApiRpsParticipant(rpsGameWithParticipants.RequestingParticipant),
+						InvitedParticipant:    ToApiRpsParticipant(rpsGameWithParticipants.InvitedParticipant),
+					},
+				},
+			}, nil
+		},
+	)
+}
