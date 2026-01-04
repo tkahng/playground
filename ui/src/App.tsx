e215e4363bce52044629460fa5ecac3e95532746
tkahng <tkahng@gmail.com>
2026-01-03 22:29:57 -0800

    feat(ui): add rock paper scissors to app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,6 @@ import AuthenticatedLayoutOutlet from "@/layouts/authenticated-layout-outlet";
 import DashboardLayout from "@/layouts/dashboard-layout";
 import PlayerLayout from "@/layouts/player-layout";
 import RootLayout from "@/layouts/root";
-import RockPaperScissors from "@/pages/account/rock-paper-scissors/rock-paper-scissors";
 import PermissionEdit from "@/pages/admin/permissions/permissions-edit";
 import PermissionListPage from "@/pages/admin/permissions/permissions-list";
 import RoleEdit from "@/pages/admin/roles/role-edit";
@@ -64,6 +63,8 @@ import TeamMembersSettingPage from "./pages/teams/settings/team-members-settings
 import TeamNotifications from "./pages/teams/settings/team-notifications";
 import TeamSettingsRedirect from "./pages/teams/team-settings-redirect";
 import UserTeamInvitationRedirectPage from "./pages/teams/user-team-invitation-redirect-page";
+import RockPaperScissors from "@/pages/account/rock-paper-scissors/rock-paper-scissors";
+import RockPaperScissorsPage from "./pages/rock-paper-scissors/rock-paper-scissors";
 
 function TeamRoutes() {
   return (
@@ -145,6 +146,10 @@ function App() {
               <Route path="/about" element={<LandingAboutPage />} />
               <Route path="/contact" element={<LandingContactPage />} />
               <Route path="/say-hello" element={<SayHelloPage />} />
+              <Route
+                path="/rock-paper-scissors"
+                element={<RockPaperScissorsPage />}
+              />
             </Route>
             {/* auth routes */}
             <Route element={<PublicLayout />}>
