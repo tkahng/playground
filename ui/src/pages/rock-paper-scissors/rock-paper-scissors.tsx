1a1a60106e6e3b93eb7bfef8a6280de38aef13e6
tkahng <tkahng@gmail.com>
2026-01-06 00:59:16 -0800

    feat(ui): update rock paper scissor move and result component

--- ui/src/pages/rock-paper-scissors/rock-paper-scissors.tsx
+++ ui/src/pages/rock-paper-scissors/rock-paper-scissors.tsx
@@ -1,32 +1,12 @@
-import { Button } from "@/components/ui/button";
-import {
-  FieldGroup,
-  FieldSet,
-  FieldDescription,
-  FieldLabel,
-  Field,
-  FieldContent,
-  FieldTitle,
-  FieldError,
-} from "@/components/ui/field";
-import { RadioGroupItem, RadioGroup } from "@/components/ui/radio-group";
 import { rpsGameQueries } from "@/lib/rps-game-queries";
 import { RpsGameWithParticipants } from "@/schema.types";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
 import { useSearchParams } from "react-router";
 import { toast } from "sonner";
-import z from "zod";
 import { GameResult } from "../account/rock-paper-scissors/game-result";
 import { ErrorCard } from "@/components/error-card";
-
-const moves = ["rock", "paper", "scissors"] as const;
-
-const submitToGameFormSchema = z.object({
-  move: z.enum(moves),
-});
+import { MoveSelection } from "../account/rock-paper-scissors/move";
 
 export default function RockPaperScissorsPage() {
   const [played, setPlayed] = useState(false);
@@ -70,89 +50,32 @@ export default function RockPaperScissorsPage() {
     },
   });
 
-  const submitToGameForm = useForm<z.infer<typeof submitToGameFormSchema>>({
-    resolver: zodResolver(submitToGameFormSchema),
-    defaultValues: { move: "rock" },
-  });
-  const onSubmit = (data: z.infer<typeof submitToGameFormSchema>) => {
-    mutation.mutate({ token: token!, move: data.move });
-  };
   if (!token) return <p>Missing token</p>;
   if (isLoading) return <p>Loading...</p>;
   if (error) return <ErrorCard />;
   if (!rpsGame) return <p>Game not found</p>;
   return (
-    <div>
-      <h1>Rock Paper Scissors</h1>
+    <div className="flex flex-col gap-4 items-center justify-center">
       <div>
         {!played && (
           <div>
-            <form
-              id="submit-game-form"
-              className="rounded-lg border p-4 space-y-2 flex flex-col items-center justify-center"
-              onSubmit={submitToGameForm.handleSubmit(onSubmit)}
-            >
-              <p className="text-lg font-bold"> Submit your move</p>
-
-              <FieldGroup className="flex flex-col gap-2 items-center justify-center">
-                <Controller
-                  name="move"
-                  control={submitToGameForm.control}
-                  render={({ field, fieldState }) => (
-                    <FieldSet data-invalid={fieldState.invalid}>
-                      <FieldDescription>Choose your move</FieldDescription>
-                      <RadioGroup
-                        name={field.name}
-                        value={field.value}
-                        onValueChange={field.onChange}
-                        className="flex flex-col sm:flex-row"
-                        aria-invalid={fieldState.invalid}
-                      >
-                        {moves.map((m) => (
-                          <FieldLabel
-                            key={m}
-                            htmlFor={`form-rhf-radiogroup-${m}`}
-                          >
-                            <Field
-                              orientation="horizontal"
-                              data-invalid={fieldState.invalid}
-                            >
-                              <FieldContent>
-                                <FieldTitle className="text-lg font-bold">
-                                  {m}
-                                </FieldTitle>
-                              </FieldContent>
-                              <RadioGroupItem
-                                value={m}
-                                id={`form-rhf-radiogroup-${m}`}
-                                aria-invalid={fieldState.invalid}
-                              />
-                            </Field>
-                          </FieldLabel>
-                        ))}
-                      </RadioGroup>
-                      {fieldState.invalid && (
-                        <FieldError errors={[fieldState.error]} />
-                      )}
-                    </FieldSet>
-                  )}
-                />
-              </FieldGroup>
-              <Button type="submit" form="submit-game-form">
-                Submit move
-              </Button>
-            </form>
+            <MoveSelection
+              handleSubmit={(move) => mutation.mutate({ token: token!, move })}
+              opponentPlayer={rpsGame?.data.requesting_participant?.player}
+            />
           </div>
         )}
         {played && game && (
-          <GameResult
-            {...{
-              result: game.invited_participant.result,
-              opponent: game.requesting_participant.player?.email || "",
-              playerMove: game.invited_participant.move,
-              opponentMove: game.requesting_participant.move,
-            }}
-          ></GameResult>
+          <div>
+            <GameResult
+              {...{
+                result: game.invited_participant.result,
+                opponent: game.requesting_participant.player?.email || "",
+                playerMove: game.invited_participant.move,
+                opponentMove: game.requesting_participant.move,
+              }}
+            />
+          </div>
         )}
         {played && !rpsGame && <div>Something went wrong...</div>}
       </div>
