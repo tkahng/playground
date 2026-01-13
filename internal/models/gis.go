d4e1ee681468ec96005c10548441168a51e5ce2d
tkahng <tkahng@gmail.com>
2026-01-12 18:47:02 -0800

    feat: remove country gis

--- internal/models/gis.go
+++ internal/models/gis.go
@@ -8,61 +8,6 @@ import (
 	"github.com/twpayne/go-geom/encoding/geojson"
 )
 
-type Country struct {
-	_     struct{} `db:"countries" schema:"gis" json:"-"`
-	GID   int64    `db:"gid,pk" json:"gid"`
-	Name  string   `db:"name" json:"name"`
-	ISOA2 string   `db:"iso_a2_eh" json:"iso_a2_eh"`
-	ISOA3 string   `db:"iso_a3_eh" json:"iso_a3_eh"`
-	Geom  geom.T   `db:"geom" json:"-"`
-}
-
-func (w *Country) MarshalJSON() ([]byte, error) {
-	geometry, err := geojson.Marshal(w.Geom)
-	if err != nil {
-		return nil, err
-	}
-	return json.Marshal(&struct {
-		GID      int64           `json:"gid"`
-		Name     string          `json:"name"`
-		ISOA2    string          `json:"iso_a2_eh"`
-		ISOA3    string          `json:"iso_a3_eh"`
-		Geometry json.RawMessage `json:"geometry"`
-	}{
-		GID:      w.GID,
-		Name:     w.Name,
-		ISOA2:    w.ISOA2,
-		ISOA3:    w.ISOA3,
-		Geometry: geometry,
-	})
-}
-
-func (w *Country) UnmarshalJSON(data []byte) error {
-	if w == nil {
-		return errors.New("models.Country: UnmarshalJSON on nil pointer")
-	}
-	var country struct {
-		GID      int64           `json:"gid"`
-		Name     string          `json:"name"`
-		ISOA2    string          `json:"iso_a2_eh"`
-		ISOA3    string          `json:"iso_a3_eh"`
-		Geometry json.RawMessage `json:"geometry"`
-	}
-	if err := json.Unmarshal(data, &country); err != nil {
-		return err
-	}
-	var geom geom.T
-	if err := geojson.Unmarshal(country.Geometry, &geom); err != nil {
-		return err
-	}
-	w.GID = country.GID
-	w.Name = country.Name
-	w.ISOA2 = country.ISOA2
-	w.ISOA3 = country.ISOA3
-	w.Geom = geom
-	return nil
-}
-
 type PopulatedPlace struct {
 	_          struct{}    `db:"populated_places" schema:"gis" json:"-"`
 	GID        int         `json:"gid" db:"gid,pk"`
--- internal/stores/gis_store_decorator.go
+++ internal/stores/gis_store_decorator.go
@@ -2,7 +2,6 @@ package stores
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/tkahng/playground/internal/database"
 	"github.com/tkahng/playground/internal/models"
@@ -10,10 +9,7 @@ import (
 )
 
 type DBGisStoreDecorator struct {
-	Delegate                GisStore
-	CreateCountryFunc       func(ctx context.Context, country *models.Country) (*models.Country, error)
-	CreateManyCountriesFunc func(ctx context.Context, countries []*models.Country) error
-	FindCountryByPointFunc  func(ctx context.Context, point *geom.Point) (*models.Country, error)
+	Delegate GisStore
 }
 
 // CreateManyPopulatedPlaces implements [GisStore].
@@ -35,39 +31,6 @@ func NewDBGisStoreDecorator(db database.Dbx) *DBGisStoreDecorator {
 	return &DBGisStoreDecorator{
 		Delegate: NewGisStore(db),
 	}
-}
-
-// CreateCountry implements [GisStore].
-func (d *DBGisStoreDecorator) CreateCountry(ctx context.Context, country *models.Country) (*models.Country, error) {
-	if d.CreateCountryFunc != nil {
-		return d.CreateCountryFunc(ctx, country)
-	}
-	if d.Delegate != nil {
-		return d.Delegate.CreateCountry(ctx, country)
-	}
-	return nil, fmt.Errorf("DBGisStoreDecorator CreateCountry %w", ErrDelegateNil)
-}
-
-// CreateManyCountries implements [GisStore].
-func (d *DBGisStoreDecorator) CreateManyCountries(ctx context.Context, countries []*models.Country) error {
-	if d.CreateManyCountriesFunc != nil {
-		return d.CreateManyCountriesFunc(ctx, countries)
-	}
-	if d.Delegate != nil {
-		return d.Delegate.CreateManyCountries(ctx, countries)
-	}
-	return fmt.Errorf("DBGisStoreDecorator CreateManyCountries %w", ErrDelegateNil)
-}
-
-// FindCountryByPoint implements [GisStore].
-func (d *DBGisStoreDecorator) FindCountryByPoint(ctx context.Context, point *geom.Point) (*models.Country, error) {
-	if d.FindCountryByPointFunc != nil {
-		return d.FindCountryByPointFunc(ctx, point)
-	}
-	if d.Delegate != nil {
-		return d.Delegate.FindCountryByPoint(ctx, point)
-	}
-	return nil, fmt.Errorf("DBGisStoreDecorator FindCountryByPoint %w", ErrDelegateNil)
 }
 
 var _ GisStore = (*DBGisStoreDecorator)(nil)
