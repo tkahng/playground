fa88479c4d24425d1b3f2143593074ffb69176eb
tkahng <tkahng@gmail.com>
2026-05-09 08:01:25 -0700

    feat: revert poller ops

--- internal/jobs/poller.go
+++ internal/jobs/poller.go
@@ -79,7 +79,7 @@ func NewDbPoller(store JobStore, dispatcher Dispatcher, opts ...PollerOptsFunc) 
 		opts: pollerOpts{
 			Interval: 5 * time.Second,
 			Timeout:  30 * time.Second,
-			Size:     10,
+			Size:     1,
 		},
 	}
 	for _, opt := range opts {
