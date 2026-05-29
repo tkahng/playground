18c4d4f6632a28cf549f613fcebd49b6ba84a69e
tkahng <tkahng@gmail.com>
2026-05-29 13:22:34 -0700

    feat(rps): reduce house cooldown to 3s

--- internal/services/gaming_rps_game_service.go
+++ internal/services/gaming_rps_game_service.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	HouseMaxBet         int64         = 500
-	HouseCooldown       time.Duration = 30 * time.Second
+	HouseCooldown       time.Duration = 3 * time.Second
 	HouseWinsMessage                  = "House always wins."
 	GameDurationSeconds int64         = 3 * 24 * 60 * 60 // 3 days
 )
